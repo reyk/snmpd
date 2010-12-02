7243f0908c9ca6b7ea4795abfdf80aa8f53546e5
Reyk Floeter <git@reykfloeter.com>
2010-12-02 12:00:00 +0100

    I didn't like the play, but I saw it under adverse conditions. The curtain was up. ... Added line 7 Id uGu4T3BW4y8= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -4,3 +4,4 @@
  * Copyright (c) 2007, 2008 Reyk Floeter <reyk@openbsd.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
