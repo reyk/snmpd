2447b92ff0a359d01390ecd89012b2f737abdc10
Reyk Floeter <git@reykfloeter.com>
2010-12-13 12:00:00 +0100

    Excellent day to have a rotten day. ... Added line 51 Id /KYBVrVeS1M= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -48,3 +48,4 @@ static ssize_t	get_id(struct ber *b, unsigned long *tag, int *class,
     int *cstruct);
 static ssize_t	get_len(struct ber *b, ssize_t *len);
 static ssize_t	ber_read_element(struct ber *ber, struct ber_element *elm);
+static ssize_t	ber_readbuf(struct ber *b, void *buf, size_t nbytes);
