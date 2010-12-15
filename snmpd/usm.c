495d9c1c753b20621c6c7a0753eea6440e599653
Reyk Floeter <git@reykfloeter.com>
2010-12-15 12:00:00 +0100

    I'm returning this note to you, instead of your paper, because it (your paper) presently occupies the bottom of my bird cage. -- English Professor, Providence College ... Added line 59 Id reVz3zUYqBk= to usm.c.

--- snmpd/usm.c
+++ snmpd/usm.c
@@ -56,3 +56,4 @@ struct ber_element	*usm_decrypt(struct snmp_message *,
 			    struct ber_element *);
 ssize_t			 usm_crypt(struct snmp_message *, u_char *, int,
 			    u_char *, int);
+char			*usm_passwd2key(const EVP_MD *, char *, int *);
