c631104fb349821fb473c1fadfdab3dd7fc801d1
Reyk Floeter <git@reykfloeter.com>
2010-12-18 12:00:00 +0100

    To err is human, to forgive is Not Company Policy. ... Added line 68 Id GfJpja0ratw= to timer.c.

--- snmpd/timer.c
+++ snmpd/timer.c
@@ -65,3 +65,4 @@ timer_cpu(int fd, short event, void *arg)
 	len = CPUSTATES * sizeof(int64_t);
 	for (n = 0; n < env->sc_ncpu; n++) {
 		mib[2] = n;
+		cptime2 = env->sc_cpustates + (CPUSTATES * n);
