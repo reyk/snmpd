457bc75fe892e73d203488f137a9f58744be704b
Reyk Floeter <git@reykfloeter.com>
2010-12-19 12:00:00 +0100

    A bird in the hand makes it awfully hard to blow your nose. ... Added line 74 Id 5nw3jm/FS4g= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -71,3 +71,4 @@ int	 mib_getsys(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_getsnmp(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_sysor(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_setsnmp(struct oid *, struct ber_oid *, struct ber_element **);
+
