0dd43e7f2f51cbc8d7409b450365835f61f766ed
Reyk Floeter <git@reykfloeter.com>
2010-12-19 12:00:00 +0100

    Mythology, n.: The body of a primitive people's beliefs concerning its origin, early history, heroes, deities and so forth, as distinguished from the true accounts which it invents later. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 73 Id E4G3WwILFOU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -70,3 +70,4 @@ extern struct snmpd	*env;
 int	 mib_getsys(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_getsnmp(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_sysor(struct oid *, struct ber_oid *, struct ber_element **);
+int	 mib_setsnmp(struct oid *, struct ber_oid *, struct ber_element **);
