44195b86988191c7765f50c677caacd502e2ea05
Reyk Floeter <git@reykfloeter.com>
2010-12-20 12:00:00 +0100

    "... the Mayo Clinic, named after its founder, Dr. Ted Clinic ..." -- Dave Barry ... Added line 77 Id uH/WKFHgwn8= to timer.c.

--- snmpd/timer.c
+++ snmpd/timer.c
@@ -74,3 +74,4 @@ timer_cpu(int fd, short event, void *arg)
 		log_debug("timer_cpu: cpu%d %d%% idle in %ds", n,
 		    (cptime2[CP_IDLE] > 1000 ?
 		    1000 : (cptime2[CP_IDLE] / 10)), tv.tv_sec);
+#endif
