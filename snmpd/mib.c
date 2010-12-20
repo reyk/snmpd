cc6b4b026e9a059b596c742be4bf45e6ba38d72b
Reyk Floeter <git@reykfloeter.com>
2010-12-20 12:00:00 +0100

    Sorry. I forget what I was going to say. ... Added line 78 Id vWFWpDhFmQ4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -75,3 +75,4 @@ int	 mib_setsnmp(struct oid *, struct ber_oid *, struct ber_element **);
 static struct oid mib_tree[] = MIB_TREE;
 static struct ber_oid zerodotzero = { { 0, 0 }, 2 };
 
+#define sizeofa(_a) (sizeof(_a) / sizeof((_a)[0]))
