2dd2ad1f3d69778a8b53d100a8204f21ed53d723
Reyk Floeter <git@reykfloeter.com>
2010-12-24 12:00:00 +0100

    There once was an old man from Esser, Who's knowledge grew lesser and lesser. It at last grew so small, He knew nothing at all, And now he's a College Professor. ... Added line 94 Id JwSJFOf7olU= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -91,3 +91,4 @@ void			 ber_set_header(struct ber_element *, int,
 void			 ber_link_elements(struct ber_element *,
 			    struct ber_element *);
 struct ber_element	*ber_unlink_elements(struct ber_element *);
+void			 ber_replace_elements(struct ber_element *,
