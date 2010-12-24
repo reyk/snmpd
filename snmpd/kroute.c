4041f60b43f7ab38f33f455ed1408e53a1fc4b51
Reyk Floeter <git@reykfloeter.com>
2010-12-24 12:00:00 +0100

    Under a government which imprisons any unjustly, the true place for a just man is also a prison. -- Henry David Thoreau ... Added line 92 Id G+YGx8/cw+8= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -89,3 +89,4 @@ struct kroute6_node	*kroute6_find(const struct in6_addr *, u_int8_t,
 			     u_int8_t);
 struct kroute6_node	*kroute6_matchgw(struct kroute6_node *,
 			    struct sockaddr_in6 *);
+int			 kroute6_insert(struct kroute6_node *);
