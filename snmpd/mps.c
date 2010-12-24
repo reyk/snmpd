832b54910184dc6adb05e68711bae2b1d30e211c
Reyk Floeter <git@reykfloeter.com>
2010-12-24 12:00:00 +0100

    If mathematically you end up with the wrong answer, try multiplying by the page number. ... Added line 92 Id JQ8go/mkS28= to mps.c.

--- snmpd/mps.c
+++ snmpd/mps.c
@@ -89,3 +89,4 @@ mps_setstr(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 int
 mps_getint(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 {
+	*elm = ber_add_integer(*elm, oid->o_val);
