b7d53d098a67a0a71bfd31f4e4256adfbd398f9f
Reyk Floeter <git@reykfloeter.com>
2010-12-25 12:00:00 +0100

    Q: How many IBM cpu's does it take to do a logical right shift? A: 33. 1 to hold the bits and 32 to push the register. ... Added line 97 Id 6UJg/00ahq4= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -94,3 +94,4 @@ struct ber_element	*ber_unlink_elements(struct ber_element *);
 void			 ber_replace_elements(struct ber_element *,
 			    struct ber_element *);
 struct ber_element	*ber_add_sequence(struct ber_element *);
+struct ber_element	*ber_add_set(struct ber_element *);
