029ccce6932965940a92632cf7256ec3a8dd2933
Reyk Floeter <git@reykfloeter.com>
2010-12-26 12:00:00 +0100

    Today is a good day to bribe a high-ranking public official. ... Added line 102 Id 9l4SjjYvbIo= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -99,3 +99,4 @@ snmpe(struct snmpd *x_env, int pipe_parent2snmpe[2])
 	if ((env->sc_sock = snmpe_bind(&env->sc_address)) == -1)
 		fatalx("snmpe: failed to bind SNMP UDP socket");
 
+	if ((pw = getpwnam(SNMPD_USER)) == NULL)
