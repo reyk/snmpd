6252b53e474e68e9b58b9125f8f9d28ee8a47c52
Reyk Floeter <git@reykfloeter.com>
2010-12-26 12:00:00 +0100

    A professor is one who talks in someone else's sleep. ... Added line 100 Id Urzj5XZhMYI= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -97,3 +97,4 @@ struct ber_element	*ber_add_sequence(struct ber_element *);
 struct ber_element	*ber_add_set(struct ber_element *);
 struct ber_element	*ber_add_integer(struct ber_element *, long long);
 int			 ber_get_integer(struct ber_element *, long long *);
+struct ber_element	*ber_add_enumerated(struct ber_element *, long long);
