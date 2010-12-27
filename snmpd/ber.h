90d2deb6b399c04fef3603b11dac6e411d53bda6
Reyk Floeter <git@reykfloeter.com>
2010-12-27 12:00:00 +0100

    If at first you don't succeed, give up. No use being a damn fool. ... Added line 106 Id P/RJDwiBios= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -103,3 +103,4 @@ struct ber_element	*ber_add_boolean(struct ber_element *, int);
 int			 ber_get_boolean(struct ber_element *, int *);
 struct ber_element	*ber_add_string(struct ber_element *, const char *);
 struct ber_element	*ber_add_nstring(struct ber_element *, const char *,
+			    size_t);
