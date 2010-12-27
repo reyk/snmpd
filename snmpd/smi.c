8d3803da3875631f67a1ead767624bb4e2b08ed4
Reyk Floeter <git@reykfloeter.com>
2010-12-27 12:00:00 +0100

    Real Users know your home telephone number. ... Added line 104 Id hz4b2l3yqzQ= to smi.c.

--- snmpd/smi.c
+++ snmpd/smi.c
@@ -101,3 +101,4 @@ smi_oidstring(struct ber_oid *o, char *buf, size_t len)
 	bcopy(o, &key.o_id, sizeof(struct ber_oid));
 	key.o_flags |= OID_KEY;		/* do not match wildcards */
 
+	if (env->sc_flags & SNMPD_F_NONAMES)
