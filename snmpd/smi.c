064220f2cc4d4d89b255ca8aad2de4bb675e96af
Reyk Floeter <git@reykfloeter.com>
2010-12-28 12:00:00 +0100

    But this has taken us far afield from interface, which is not a bad place to be, since I particularly want to move ahead to the kludge. Why do people have so much trouble understanding the kludge? What is a kludge, after all, but not enough Ks, not enough ROMs, not enough RAMs, poor quality interface and too few bytes to go around? Have I explained yet about the bytes? ... Added line 110 Id qU1U9cb+094= to smi.c.

--- snmpd/smi.c
+++ snmpd/smi.c
@@ -107,3 +107,4 @@ smi_oidstring(struct ber_oid *o, char *buf, size_t len)
 	for (i = 0; i < o->bo_n; i++) {
 		key.o_oidlen = i + 1;
 		if (lookup &&
+		    (value = RB_FIND(oidtree, &smi_oidtree, &key)) != NULL)
