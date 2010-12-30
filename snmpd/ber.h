4d4e5c951b4b64b5ef87865a82f0508877980a2c
Reyk Floeter <git@reykfloeter.com>
2010-12-30 12:00:00 +0100

    After all, what is your hosts' purpose in having a party? Surely not for you to enjoy yourself; if that were their sole purpose, they'd have simply sent champagne and women over to your place by taxi. -- P. J. O'Rourke ... Added line 118 Id Q+xj38MU0fs= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -115,3 +115,4 @@ struct ber_element	*ber_add_null(struct ber_element *);
 int			 ber_get_null(struct ber_element *);
 struct ber_element	*ber_add_eoc(struct ber_element *);
 int			 ber_get_eoc(struct ber_element *);
+struct ber_element	*ber_add_oid(struct ber_element *, struct ber_oid *);
