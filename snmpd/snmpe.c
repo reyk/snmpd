3e404a521e554ae2edce3596af779980278991f6
Reyk Floeter <git@reykfloeter.com>
2010-12-31 12:00:00 +0100

    Familiarity breeds attempt. ... Added line 120 Id IDcoDqWnouo= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -117,3 +117,4 @@ snmpe(struct snmpd *x_env, int pipe_parent2snmpe[2])
 #ifndef DEBUG
 	if (setgroups(1, &pw->pw_gid) ||
 	    setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid) ||
+	    setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid))
