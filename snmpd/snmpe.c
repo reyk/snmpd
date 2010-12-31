c91e7c0dc7a31007478c21f3d892a9c2c4bfb6ab
Reyk Floeter <git@reykfloeter.com>
2010-12-31 12:00:00 +0100

    Bolub's Fourth Law of Computerdom: Project teams detest weekly progress reporting because it so vividly manifests their lack of progress. ... Added line 121 Id KmQflvDIuEQ= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -118,3 +118,4 @@ snmpe(struct snmpd *x_env, int pipe_parent2snmpe[2])
 	if (setgroups(1, &pw->pw_gid) ||
 	    setresgid(pw->pw_gid, pw->pw_gid, pw->pw_gid) ||
 	    setresuid(pw->pw_uid, pw->pw_uid, pw->pw_uid))
+		fatal("snmpe: cannot drop privileges");
