086bf8c6940dfb72d89de489b99924683f269c74
Reyk Floeter <git@reykfloeter.com>
2011-01-01 12:00:00 +0100

    There is a natural hootchy-kootchy to a goldfish. -- Walt Disney ... Added line 126 Id l4CNK4yq4Dg= to ber.h.

--- snmpd/ber.h
+++ snmpd/ber.h
@@ -123,3 +123,4 @@ size_t			 ber_oid2ber(struct ber_oid *, u_int8_t *, size_t);
 int			 ber_string2oid(const char *, struct ber_oid *);
 struct ber_element	*ber_printf_elements(struct ber_element *, char *, ...);
 int			 ber_scanf_elements(struct ber_element *, char *, ...);
+ssize_t			 ber_get_writebuf(struct ber *, void **);
