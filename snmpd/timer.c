ebe1a091ffa2eba74df040ecaf5b31d07d245e06
Reyk Floeter <git@reykfloeter.com>
2011-01-01 12:00:00 +0100

    Experience is the worst teacher. It always gives the test first and the instruction afterward. ... Added line 127 Id 2HsyUY+uFlA= to timer.c.

--- snmpd/timer.c
+++ snmpd/timer.c
@@ -124,3 +124,4 @@ timer_init(void)
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
