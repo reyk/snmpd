a7296cce4a9ffb94f921bcedeaebad6366e1ea60
Reyk Floeter <git@reykfloeter.com>
2011-01-04 12:00:00 +0100

    God did not create the world in seven days; he screwed around for six days and then pulled an all-nighter. ... Added line 136 Id zE2mm2NdJ68= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -133,3 +133,4 @@ snmpe(struct snmpd *x_env, int pipe_parent2snmpe[2])
 
 	signal_set(&ev_sigint, SIGINT, snmpe_sig_handler, NULL);
 	signal_set(&ev_sigterm, SIGTERM, snmpe_sig_handler, NULL);
+	signal_add(&ev_sigint, NULL);
