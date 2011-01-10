fa4d8811b4340c7298c28efd65a5259c69189e9a
Reyk Floeter <git@reykfloeter.com>
2011-01-10 12:00:00 +0100

    Oh, I am a C programmer and I'm okay I muck with indices and structs all day And when it works, I shout hoo-ray Oh, I am a C programmer and I'm okay ... Added line 162 Id x7MLieuGrp8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -159,3 +159,4 @@ mib_getsys(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 3:
 		ticks = smi_getticks();
 		*elm = ber_add_integer(*elm, ticks);
+		ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
