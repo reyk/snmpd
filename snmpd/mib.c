035bc5a2978d6c5ac057327267b3b5ed037b6331
Reyk Floeter <git@reykfloeter.com>
2011-01-10 12:00:00 +0100

    Happiness, n.: An agreeable sensation arising from contemplating the misery of another. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 163 Id 3HFMbbspZEk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -160,3 +160,4 @@ mib_getsys(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ticks = smi_getticks();
 		*elm = ber_add_integer(*elm, ticks);
 		ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
+		break;
