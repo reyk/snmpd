872fae2b985e82d0383ca61e93f0856502327d42
Reyk Floeter <git@reykfloeter.com>
2011-01-13 12:00:00 +0100

    A witty saying proves nothing, but saying something pointless gets people's attention. ... Added line 175 Id YSLMBb8b4vU= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -172,3 +172,4 @@ kr_init(void)
 		    rcvbuf > default_rcvbuf &&
 		    setsockopt(kr_state.ks_fd, SOL_SOCKET, SO_RCVBUF,
 		    &rcvbuf, sizeof(rcvbuf)) == -1 && errno == ENOBUFS;
+		    rcvbuf /= 2)
