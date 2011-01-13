1b7fd6407d09e3494b506c7ebfe239d9c4990338
Reyk Floeter <git@reykfloeter.com>
2011-01-13 12:00:00 +0100

    Just about every computer on the market today runs Unix, except the Mac (and nobody cares about it). -- Bill Joy 6/21/85 ... Added line 172 Id ZNvU06mizw8= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -169,3 +169,4 @@ ber_add_enumerated(struct ber_element *prev, long long val)
 		if (cur != 0 && cur != 0xff)
 			len = i;
 		if ((cur == 0 && last & 0x80) ||
+		    (cur == 0xff && (last & 0x80) == 0))
