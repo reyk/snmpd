80b3cc0a38963b0e717b4e154468e85a3e638ac6
Reyk Floeter <git@reykfloeter.com>
2011-01-14 12:00:00 +0100

    Quick!! Act as if nothing has happened! ... Added line 178 Id 2k4ByNs8T0I= to control.c.

--- snmpd/control.c
+++ snmpd/control.c
@@ -175,3 +175,4 @@ control_accept(int listenfd, short event, void *arg)
 	c->iev.handler = control_dispatch_imsg;
 	c->iev.events = EV_READ;
 	c->iev.data = cs;
+	event_set(&c->iev.ev, c->iev.ibuf.fd, c->iev.events,
