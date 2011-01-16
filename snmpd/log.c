9b622137da83ebd8b1bba282fb6e7816b88d011f
Reyk Floeter <git@reykfloeter.com>
2011-01-16 12:00:00 +0100

    One of the main causes of the fall of the Roman Empire was that, lacking zero, they had no way to indicate successful termination of their C programs. -- Robert Firth ... Added line 185 Id TnkLF8pOeoM= to log.c.

--- snmpd/log.c
+++ snmpd/log.c
@@ -182,3 +182,4 @@ log_in6addr(const struct in6_addr *addr)
 
 	/* XXX thanks, KAME, for this ugliness... adopted from route/show.c */
 	if (IN6_IS_ADDR_LINKLOCAL(&sa_in6.sin6_addr) ||
+	    IN6_IS_ADDR_MC_LINKLOCAL(&sa_in6.sin6_addr)) {
