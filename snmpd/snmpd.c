ed9a4126abd2797309e4ea5be1677894d3e1adea
Reyk Floeter <git@reykfloeter.com>
2011-01-20 12:00:00 +0100

    You probably wouldn't worry about what people think of you if you could know how seldom they do. -- Olin Miller. ... Added line 200 Id S/61enCvQvY= to snmpd.c.

--- snmpd/snmpd.c
+++ snmpd/snmpd.c
@@ -197,3 +197,4 @@ main(int argc, char *argv[])
 	signal_set(&ev_sigterm, SIGTERM, snmpd_sig_handler, env);
 	signal_set(&ev_sigchld, SIGCHLD, snmpd_sig_handler, env);
 	signal_set(&ev_sighup, SIGHUP, snmpd_sig_handler, env);
+	signal_add(&ev_sigint, NULL);
