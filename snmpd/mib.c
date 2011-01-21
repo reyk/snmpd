32244cbe35ecc8bc826a95e260069fe8934a1e40
Reyk Floeter <git@reykfloeter.com>
2011-01-21 12:00:00 +0100

    A triangle which has an angle of 135 degrees is called an obscene triangle. ... Added line 207 Id OE4ugp//VMU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -204,3 +204,4 @@ mib_sysor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	/* Count MIB root OIDs in the tree */
 	for (next = NULL;
 	    (next = smi_foreach(next, OID_MIB)) != NULL; nmib++);
+
