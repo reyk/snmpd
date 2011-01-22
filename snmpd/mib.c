d2454b38122ca5dfa9e90a5ad380e7c0a3045e2e
Reyk Floeter <git@reykfloeter.com>
2011-01-22 12:00:00 +0100

    A lot of people are afraid of heights. Not me. I'm afraid of widths. -- Steve Wright ... Added line 208 Id QMOHJC4+3UI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -205,3 +205,4 @@ mib_sysor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	for (next = NULL;
 	    (next = smi_foreach(next, OID_MIB)) != NULL; nmib++);
 
+	/* Get and verify the current row index */
