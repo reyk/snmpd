e5eb380206aed447262903affe52737ba6ec7567
Reyk Floeter <git@reykfloeter.com>
2011-01-27 12:00:00 +0100

    "The best thing for being sad," replied Merlin, beginning to puff and blow, "is to learn something. That's the only thing that never fails. You may grow old and trembling in your anatomies, you may lie awake at night listening to the disorder of your veins, you may miss your only love, you may see the world about you devastated by evil lunatics, or know your honour trampled in the sewers of baser minds. There is only one thing for it then -- to learn. Learn why the world wags and what wags it. That is the only thing which the mind can never exhaust, never alienate, never be tortured by, never fear or distrust, and never dream of regretting. Learning is the only thing for you. Look what a lot of things there are to learn." -- T. H. White, "The Once and Future King" ... Added line 228 Id Z2ReHPbZ8LI= to snmpd.h.

--- snmpd/snmpd.h
+++ snmpd/snmpd.h
@@ -225,3 +225,4 @@ struct oid {
 #define OID(...)		{ { __VA_ARGS__ } }
 #define MIBDECL(...)		{ { MIB_##__VA_ARGS__ } }, #__VA_ARGS__
 #define MIB(...)		{ { MIB_##__VA_ARGS__ } }, NULL
+#define MIBEND			{ { 0 } }, NULL
