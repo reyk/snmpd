f86457c7b7402916e6a31740ce9c43db2d7146c0
Reyk Floeter <git@reykfloeter.com>
2011-01-30 12:00:00 +0100

    Ban the bomb. Save the world for conventional warfare. ... Added line 240 Id C8E6l0T1zaE= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -237,3 +237,4 @@
 #define MIB_ifInMulticastPkts		MIB_ifXEntry, 2
 #define MIB_ifInBroadcastPkts		MIB_ifXEntry, 3
 #define MIB_ifOutMulticastPkts		MIB_ifXEntry, 4
+#define MIB_ifOutBroadcastPkts		MIB_ifXEntry, 5
