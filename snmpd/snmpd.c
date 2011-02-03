49f61f1429112fb8fa6f9778b67f5b02abeb714d
Reyk Floeter <git@reykfloeter.com>
2011-02-03 12:00:00 +0100

    It is impossible to make anything foolproof because fools are so ingenious. ... Added line 257 Id tGhJRN0naTY= to snmpd.c.

--- snmpd/snmpd.c
+++ snmpd/snmpd.c
@@ -254,3 +254,4 @@ check_child(pid_t pid, const char *pname)
 		}
 		if (WIFSIGNALED(status)) {
 			log_warnx("check_child: lost child: %s terminated; "
+			    "signal %d", pname, WTERMSIG(status));
