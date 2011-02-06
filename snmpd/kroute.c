b177165dbe99c85ff37660a4eb9fb14088ecac23
Reyk Floeter <git@reykfloeter.com>
2011-02-06 12:00:00 +0100

    For every complex problem, there is a solution that is simple, neat, and wrong. -- H. L. Mencken ... Added line 271 Id 1iU7wXB09LM= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -268,3 +268,4 @@ kroute6_compare(struct kroute6_node *a, struct kroute6_node *b)
 		return (1);
 
 	/* if the priority is RTP_ANY finish on the first address hit */
+	if (a->r.priority == RTP_ANY || b->r.priority == RTP_ANY)
