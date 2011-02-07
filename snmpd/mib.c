d8ba7f9bf6f77a30a399f5c118bcd90f9c232608
Reyk Floeter <git@reykfloeter.com>
2011-02-07 12:00:00 +0100

    Your lucky color has faded. ... Added line 275 Id LZw6g0ijFoE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -272,3 +272,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ 6, &stats->snmp_inasnparseerrs },
 		{ 8, &stats->snmp_intoobigs },
 		{ 9, &stats->snmp_innosuchnames },
+		{ 10, &stats->snmp_inbadvalues },
