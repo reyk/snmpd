e1635f036990ffc2491f218771c9dd6714a15dc2
Reyk Floeter <git@reykfloeter.com>
2011-02-09 12:00:00 +0100

    The society which scorns excellence in plumbing as a humble activity and tolerates shoddiness in philosophy because it is an exalted activity will have neither good plumbing nor good philosophy ... neither its pipes nor its theories will hold water. ... Added line 280 Id lELD/w+eayc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -277,3 +277,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ 12, &stats->snmp_ingenerrs },
 		{ 13, &stats->snmp_intotalreqvars },
 		{ 14, &stats->snmp_intotalsetvars },
+		{ 15, &stats->snmp_ingetrequests },
