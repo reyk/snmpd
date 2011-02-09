e3eca1ba7adee397a441b87240b6eb84a80b7950
Reyk Floeter <git@reykfloeter.com>
2011-02-09 12:00:00 +0100

    A crusader's wife slipped from the garrison And had an affair with a Saracen. She was not oversexed, Or jealous or vexed, She just wanted to make a comparison. ... Added line 283 Id 8o6o+zShOOo= to control.c.

--- snmpd/control.c
+++ snmpd/control.c
@@ -280,3 +280,4 @@ control_dispatch_imsg(int fd, short event, void *arg)
 		case IMSG_CTL_NOTIFY:
 			if (c->flags & CTL_CONN_NOTIFY) {
 				log_debug("control_dispatch_imsg: "
+				    "client requested notify more than once");
