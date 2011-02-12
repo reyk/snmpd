99da53c529f30d7aca1cb4fdc7216596fa8f2558
Reyk Floeter <git@reykfloeter.com>
2011-02-12 12:00:00 +0100

    Hug O' War I will not play at tug o' war. I'd rather play at hug o' war, Where everyone hugs Instead of tugs, Where everyone giggles And rolls on the rug, Where everyone kisses, And everyone grins, And everyone cuddles, And everyone wins. -- Shel Silverstein ... Added line 294 Id Zsa+AooTkbY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -291,3 +291,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ 27, &stats->snmp_outsetrequests },
 		{ 28, &stats->snmp_outgetresponses },
 		{ 29, &stats->snmp_outtraps },
+		{ 31, &stats->snmp_silentdrops },
