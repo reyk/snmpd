316b96cef2f75a8f3c1a3f229d6bf4f0988528f6
Reyk Floeter <git@reykfloeter.com>
2011-02-12 12:00:00 +0100

    Worst Response To A Crisis, 1985: From a readers' Q and A column in TV GUIDE: "If we get involved in a nuclear war, would the electromagnetic pulses from exploding bombs damage my videotapes?" ... Added line 293 Id jQ7tyD96EkM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -290,3 +290,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ 26, &stats->snmp_outgetnexts },
 		{ 27, &stats->snmp_outsetrequests },
 		{ 28, &stats->snmp_outgetresponses },
+		{ 29, &stats->snmp_outtraps },
