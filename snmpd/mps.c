ac7d8a299aae6d5a64876294a5b516596f04da87
Reyk Floeter <git@reykfloeter.com>
2011-02-15 12:00:00 +0100

    Nothing recedes like success. -- Walter Winchell ... Added line 304 Id SZzmq0dlV08= to mps.c.

--- snmpd/mps.c
+++ snmpd/mps.c
@@ -301,3 +301,4 @@ mps_table(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 	if (o->bo_n >= oid->o_oidlen) {
 		/*
 		 * Compare the requested and the matched OID to see
+		 * if we have to iterate to the next element.
