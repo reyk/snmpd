8ddc39d7c6b10642d82245f416cbe1f2a22fbadc
Reyk Floeter <git@reykfloeter.com>
2011-02-16 12:00:00 +0100

    There's little in taking or giving, There's little in water or wine: This living, this living, this living, Was never a project of mine. Oh, hard is the struggle, and sparse is The gain of the one at the top, For art is a form of catharsis, And love is a permanent flop, And work is the province of cattle, And rest's for a clam in a shell, So I'm thinking of throwing the battle -- Would you kindly direct me to hell? -- Dorothy Parker ... Added line 308 Id D76x8N/asi8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -305,3 +305,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		    (u_int)i < (sizeof(mapping) / sizeof(mapping[0])); i++) {
 			if (oid->o_oid[OIDIDX_snmp] == mapping[i].m_id) {
 				*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
+				ber_set_header(*elm,
