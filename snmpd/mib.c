7b58ac19fccd7c592e6a798156bfff455a9a432e
Reyk Floeter <git@reykfloeter.com>
2011-02-16 12:00:00 +0100

    "Do you think what we're doing is wrong?" "Of course it's wrong! It's illegal!" "I've never done anything illegal before." "I thought you said you were an accountant!" ... Added line 309 Id GFKI+LkVOe4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -306,3 +306,4 @@ mib_getsnmp(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			if (oid->o_oid[OIDIDX_snmp] == mapping[i].m_id) {
 				*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
 				ber_set_header(*elm,
+				    BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
