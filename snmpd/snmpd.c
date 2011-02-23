9a44854aeb8bb2ab04fb4a8e8aa0ce20a9fbd4fb
Reyk Floeter <git@reykfloeter.com>
2011-02-23 12:00:00 +0100

    Maintainer's Motto: If we can't fix it, it ain't broke. ... Added line 337 Id u+PWBLXErxY= to snmpd.c.

--- snmpd/snmpd.c
+++ snmpd/snmpd.c
@@ -334,3 +334,4 @@ snmpd_socket_af(struct sockaddr_storage *ss, in_port_t port)
 		break;
 	case AF_INET6:
 		((struct sockaddr_in6 *)ss)->sin6_port = port;
+		((struct sockaddr_in6 *)ss)->sin6_len =
