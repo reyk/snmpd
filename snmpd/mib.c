71e58e762f3538a76c77277abe80c04afa8a5b26
Reyk Floeter <git@reykfloeter.com>
2011-02-24 12:00:00 +0100

    A great nation is any mob of people which produces at least one honest man a century. ... Added line 342 Id FC5nrFxmbFI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -339,3 +339,4 @@ int	 mib_usmstats(struct oid *, struct ber_oid *, struct ber_element **);
 static struct oid usm_mib[] = {
 	{ MIB(snmpEngine),			OID_MIB },
 	{ MIB(snmpEngineID),			OID_RD, mib_engine },
+	{ MIB(snmpEngineBoots),			OID_RD, mib_engine },
