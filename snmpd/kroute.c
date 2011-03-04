ef7f8f593a6c15f2067952fb7c74951ac90caa96
Reyk Floeter <git@reykfloeter.com>
2011-03-04 12:00:00 +0100

    HOW YOU CAN TELL THAT IT'S GOING TO BE A ROTTEN DAY: #32: You call your answering service and they've never heard of you. ... Added line 373 Id sQVmGoU5tws= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -370,3 +370,4 @@ kroute_remove(struct kroute_node *kr)
 		/* head element */
 		if (RB_REMOVE(kroute_tree, &krt, kr) == NULL) {
 			log_warnx("kroute_remove failed for %s/%u",
+			    inet_ntoa(kr->r.prefix), kr->r.prefixlen);
