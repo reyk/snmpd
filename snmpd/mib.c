a937061f8354cefc4f32daec77d1b99de48497c2
Reyk Floeter <git@reykfloeter.com>
2011-03-09 12:00:00 +0100

    The superfluous is very necessary. -- Voltaire ... Added line 392 Id sUuPijbRwuo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -389,3 +389,4 @@ mib_usmstats(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ OIDVAL_usmErrUserName,	&stats->snmp_usmnosuchuser },
 		{ OIDVAL_usmErrEngineId,	&stats->snmp_usmnosuchengine },
 		{ OIDVAL_usmErrDigest,		&stats->snmp_usmwrongdigest },
+		{ OIDVAL_usmErrDecrypt,		&stats->snmp_usmdecrypterr },
