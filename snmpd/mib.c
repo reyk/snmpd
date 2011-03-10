24b90a409c7b4a531ac4a18ec4d2c260b41cd39b
Reyk Floeter <git@reykfloeter.com>
2011-03-10 12:00:00 +0100

    Man is the best computer we can put aboard a spacecraft ... and the only one that can be mass produced with unskilled labor. -- Wernher von Braun ... Added line 399 Id YUXqTMZGQAo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -396,3 +396,4 @@ mib_usmstats(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		if (oid->o_oid[OIDIDX_usmStats] == mapping[i].m_id) {
 			*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
 			ber_set_header(*elm, BER_CLASS_APPLICATION,
+			    SNMP_T_COUNTER32);
