c91926099cbd0e47dea797e3a5417ef34232f37f
Reyk Floeter <git@reykfloeter.com>
2011-03-17 12:00:00 +0100

    Entropy isn't what it used to be. ... Added line 426 Id z4xTPkWM/kI= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -423,3 +423,4 @@ ber_string2oid(const char *oidstr, struct ber_oid *o)
 	bzero(o, sizeof(*o));
 
 	/* Parse OID strings in the common forms n.n.n, n_n_n_n, or n-n-n */
+	for (p = sp = str; p != NULL; sp = p) {
