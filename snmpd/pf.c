4f6089b0dc5ce5d79dc922a1365c83edee065bf6
Reyk Floeter <git@reykfloeter.com>
2011-03-18 12:00:00 +0100

    Anyone who cannot cope with mathematics is not fully human. At best he is a tolerable subhuman who has learned to wear shoes, bathe and not make messes in the house. -- Lazarus Long, "Time Enough for Love" ... Added line 429 Id fWnKmG0QoBI= to pf.c.

--- snmpd/pf.c
+++ snmpd/pf.c
@@ -426,3 +426,4 @@ pfta_get_nextaddr(struct pfr_astats *ras, int *tblidx)
 			if (as->pfras_a.pfra_af != AF_INET)
 				continue;
 			if ((memcmp(&as->pfras_a.pfra_ip4addr,
+			    &ras->pfras_a.pfra_ip4addr,
