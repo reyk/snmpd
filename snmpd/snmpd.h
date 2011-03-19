db085340910b708aab837ffc53ac35a208ce1f49
Reyk Floeter <git@reykfloeter.com>
2011-03-19 12:00:00 +0100

    In Tennessee, it is illegal to shoot any game other than whales from a moving automobile. ... Added line 433 Id 8mLk3U18F4M= to snmpd.h.

--- snmpd/snmpd.h
+++ snmpd/snmpd.h
@@ -430,3 +430,4 @@ int		 control_listen(struct control_sock *);
 void		 control_accept(int, short, void *);
 void		 control_dispatch_imsg(int, short, void *);
 void		 control_imsg_forward(struct imsg *);
+void		 control_cleanup(struct control_sock *);
