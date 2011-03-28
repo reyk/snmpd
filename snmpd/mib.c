0e5c2b96b0a727496e5b6ff606a4c4168b2902cc
Reyk Floeter <git@reykfloeter.com>
2011-03-28 12:00:00 +0200

    You will feel hungry again in another hour. ... Added line 471 Id 3zbputeI1bI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -468,3 +468,4 @@ mib_hrsystemuptime(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 
 	*elm = ber_add_integer(*elm, (now - boottime.tv_sec) * 100);
 	ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
+
