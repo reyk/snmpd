c2a90fe91128864daae93168f3ca249cb5167f0e
Reyk Floeter <git@reykfloeter.com>
2011-03-29 12:00:00 +0200

    Birth, n.: The first and direst of all disasters. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 473 Id rTQXyI/bJSY= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -470,3 +470,4 @@
 #define MIB_pfCntMatch			MIB_pfCounters, 1
 #define MIB_pfCntBadOffset		MIB_pfCounters, 2
 #define MIB_pfCntFragment		MIB_pfCounters, 3
+#define MIB_pfCntShort			MIB_pfCounters, 4
