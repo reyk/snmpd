66847699291ef21a1d32b8b2a592c928aba143a1
Reyk Floeter <git@reykfloeter.com>
2011-03-31 12:00:00 +0200

    The University of California Bears announced the signing of Reggie Philbin to a letter of intent to attend Cal next Fall. Philbin is said to make up for no talent by cheating well. Says Philbin of his decision to attend Cal, "I'm in it for the free ride." ... Added line 483 Id zh6J8bsjFzw= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -480,3 +480,4 @@
 #define MIB_pfCntStateMismatch		MIB_pfCounters, 11
 #define MIB_pfCntStateInsert		MIB_pfCounters, 12
 #define MIB_pfCntStateLimit		MIB_pfCounters, 13
+#define MIB_pfCntSrcLimit		MIB_pfCounters, 14
