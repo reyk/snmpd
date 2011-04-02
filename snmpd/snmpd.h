bb54dbe28b25ee251e4024e968fca863a2bfd932
Reyk Floeter <git@reykfloeter.com>
2011-04-02 12:00:00 +0200

    A city is a large community where people are lonesome together. -- Herbert Prochnow ... Added line 490 Id 7b87licQBKk= to snmpd.h.

--- snmpd/snmpd.h
+++ snmpd/snmpd.h
@@ -487,3 +487,4 @@ struct ber_element *
 		 mps_getnextreq(struct ber_element *, struct ber_oid *);
 int		 mps_setreq(struct ber_element *, struct ber_oid *);
 int		 mps_set(struct ber_oid *, void *, long long);
+int		 mps_getstr(struct oid *, struct ber_oid *,
