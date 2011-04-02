84bc9877c7e5d7b2b4524e50eedb0a8460f56125
Reyk Floeter <git@reykfloeter.com>
2011-04-02 12:00:00 +0200

    I went on to test the program in every way I could devise. I strained it to expose its weaknesses. I ran it for high-mass stars and low-mass stars, for stars born exceedingly hot and those born relatively cold. I ran it assuming the superfluid currents beneath the crust to be absent -- not because I wanted to know the answer, but because I had developed an intuitive feel for the answer in this particular case. Finally I got a run in which the computer showed the pulsar's temperature to be less than absolute zero. I had found an error. I chased down the error and fixed it. Now I had improved the program to the point where it would not run at all. -- George Greenstein, "Frozen Star: Of Pulsars, Black Holes and the Fate of Stars" ... Added line 491 Id DOtDAkA7RwQ= to snmpd.h.

--- snmpd/snmpd.h
+++ snmpd/snmpd.h
@@ -488,3 +488,4 @@ struct ber_element *
 int		 mps_setreq(struct ber_element *, struct ber_oid *);
 int		 mps_set(struct ber_oid *, void *, long long);
 int		 mps_getstr(struct oid *, struct ber_oid *,
+		    struct ber_element **);
