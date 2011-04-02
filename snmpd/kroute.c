ea711ed9848b6a4261cd6130075202d402cdb7cd
Reyk Floeter <git@reykfloeter.com>
2011-04-02 12:00:00 +0200

    All true wisdom is found on T-shirts. ... Added line 491 Id kA8Z53dudPo= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -488,3 +488,4 @@ kroute6_remove(struct kroute6_node *kr)
 		}
 		if (kr->next != NULL) {
 			if (RB_INSERT(kroute6_tree, &krt6, kr->next) != NULL) {
+				log_warnx("kroute6_remove failed to add %s/%u",
