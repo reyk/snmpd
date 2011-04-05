3f8a4ac89938546ba578b46c61a0c128fcdbff5f
Reyk Floeter <git@reykfloeter.com>
2011-04-05 12:00:00 +0200

    No one can make you feel inferior without your consent. -- Eleanor Roosevelt ... Added line 503 Id bKnenf6puDA= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -500,3 +500,4 @@
 #define MIB_pfLogIfIp6BytesOut		MIB_pfLogInterface, 9
 #define MIB_pfLogIfIp6PktsInPass	MIB_pfLogInterface, 10
 #define MIB_pfLogIfIp6PktsInDrop	MIB_pfLogInterface, 11
+#define MIB_pfLogIfIp6PktsOutPass	MIB_pfLogInterface, 12
