1c57262e1e9b0ae3db1555d24c4719efaeab4649
Reyk Floeter <git@reykfloeter.com>
2011-04-10 12:00:00 +0200

    "If I am elected, the concrete barriers around the WHITE HOUSE will be replaced by tasteful foam replicas of ANN MARGARET!" ... Added line 521 Id 8wDJQLkrnrM= to snmpd.h.

--- snmpd/snmpd.h
+++ snmpd/snmpd.h
@@ -518,3 +518,4 @@ int			 pft_get(struct pfr_buffer *, struct pfr_table *);
 int			 pft_count(void);
 int			 pft_get_table(struct pfr_tstats *, int);
 int			 pfta_get(struct pfr_buffer *, struct pfr_table *);
+int			 pfta_get_addr(struct pfr_astats *, int);
