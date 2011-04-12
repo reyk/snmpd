8f6edc52198bd4d73a7af6309de433d6686f29a5
Reyk Floeter <git@reykfloeter.com>
2011-04-12 12:00:00 +0200

    The income tax has made more liars out of the American people than golf has. Even when you make a tax form out on the level, you don't know when it's through if you are a crook or a martyr. -- Will Rogers ... Added line 530 Id v56vXD4bRgs= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -527,3 +527,4 @@ mib_hrsystemprocs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			return (-1);
 
 		*elm = ber_add_integer(*elm, val);
+		ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
