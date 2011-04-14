f34849e40dbbade1db31acb3ab572bdccfd0d2b6
Reyk Floeter <git@reykfloeter.com>
2011-04-14 12:00:00 +0200

    "Ah, you know the type. They like to blame it all on the Jews or the Blacks, 'cause if they couldn't, they'd have to wake up to the fact that life's one big, scary, glorious, complex and ultimately unfathomable crapshoot -- and the only reason THEY can't seem to keep up is they're a bunch of misfits and losers." -- A analysis of Neo-Nazis, from "The Badger" comic ... Added line 539 Id mXT5XHSONhA= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -536,3 +536,4 @@ snmpe_parse(struct sockaddr_storage *ss,
 			errstr = "unsupported security model";
 			stats->snmp_usmbadseclevel++;
 			msg->sm_usmerr = OIDVAL_usmErrSecLevel;
+			goto parsefail;
