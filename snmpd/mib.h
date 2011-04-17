c837b435aa01d0528a4d24bc69a9f3db9e0eeb40
Reyk Floeter <git@reykfloeter.com>
2011-04-17 12:00:00 +0200

    A new supply of round tuits has arrived and are available from Mary. Anyone who has been putting off work until they got a round tuit now has no excuse for further procrastination. ... Added line 551 Id jmWae3YWXLk= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -548,3 +548,4 @@
 #define MIB_pfIfIn4PassPkts		MIB_pfIfEntry, 6
 #define MIB_pfIfIn4PassBytes		MIB_pfIfEntry, 7
 #define MIB_pfIfIn4BlockPkts		MIB_pfIfEntry, 8
+#define MIB_pfIfIn4BlockBytes		MIB_pfIfEntry, 9
