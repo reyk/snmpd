61644152d41558a74d771a8e31505a483eff0d42
Reyk Floeter <git@reykfloeter.com>
2011-04-21 12:00:00 +0200

    Impartial, adj.: Unable to perceive any promise of personal advantage from espousing either side of a controversy or adopting either of two conflicting opinions. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 564 Id ETclklw8Oxg= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -561,3 +561,4 @@
 #define MIB_pfIfOut6PassBytes		MIB_pfIfEntry, 19
 #define MIB_pfIfOut6BlockPkts		MIB_pfIfEntry, 20
 #define MIB_pfIfOut6BlockBytes		MIB_pfIfEntry, 21
+#define MIB_pfTables			MIB_pfMIBObjects, 9
