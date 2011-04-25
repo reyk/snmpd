45245ad4d8b6a0d73583851919a9783d05c2c9f2
Reyk Floeter <git@reykfloeter.com>
2011-04-25 12:00:00 +0200

    Harrisberger's Fourth Law of the Lab: Experience is directly proportional to the amount of equipment ruined. ... Added line 582 Id JAmb0ivpZKc= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -579,3 +579,4 @@
 #define MIB_pfTblInBlockPkts		MIB_pfTblEntry, 10
 #define MIB_pfTblInBlockBytes		MIB_pfTblEntry, 11
 #define MIB_pfTblInXPassPkts		MIB_pfTblEntry, 12
+#define MIB_pfTblInXPassBytes		MIB_pfTblEntry, 13
