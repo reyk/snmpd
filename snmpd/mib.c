2704db8d700eaf690866e59a7445f3c3bb781b95
Reyk Floeter <git@reykfloeter.com>
2011-04-25 12:00:00 +0200

    Try to be the best of whatever you are, even if what you are is no good. ... Added line 581 Id Df6TSYuuh+s= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -578,3 +578,4 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	static struct ber_oid	*sop, so[] = {
 		{ { MIB_hrStorageOther } },
 		{ { MIB_hrStorageRam } },
+		{ { MIB_hrStorageVirtualMemory } },
