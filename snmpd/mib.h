3f35a96b0832c5b56e6916ca6e49f7d4346a7de6
Reyk Floeter <git@reykfloeter.com>
2011-04-27 12:00:00 +0200

    Section 2.4.3.5 AWNS (Acceptor Wait for New Cycle State). In AWNS the AH function indicates that it has received a multiline message byte. In AWNS the RFD message must be sent false and the DAC message must be sent passive true. The AH function must exit the AWNS and enter: (1) The ANRS if DAV is false (2) The AIDS if the ATN message is false and neither: (a) The LADS is active (b) Nor LACS is active -- from the IEEE Standard Digital Interface for Programmable Instrumentation ... Added line 591 Id 6iFFLPqTyKg= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -588,3 +588,4 @@
 #define MIB_pfTblOutXPassBytes		MIB_pfTblEntry, 19
 #define MIB_pfTblStatsCleared		MIB_pfTblEntry, 20
 #define MIB_pfTblAddrTable		MIB_pfTables, 129
+#define MIB_pfTblAddrEntry		MIB_pfTblAddrTable, 1
