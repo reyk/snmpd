b89c850d750f8c726b464af4a3650e4e1b869eb0
Reyk Floeter <git@reykfloeter.com>
2011-04-28 12:00:00 +0200

    Conscience is a mother-in-law whose visit never ends. -- H. L. Mencken ... Added line 593 Id hTdKvjs/S/M= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -590,3 +590,4 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	mib[1] = HW_USERMEM64;
 	len = sizeof(realmem);
 	if (sysctl(mib, sizeofa(mib), &realmem, &len, NULL, 0) == -1)
+		return (-1);
