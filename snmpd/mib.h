93df97cf1066a93dbde8648012190fbd7e98f4db
Reyk Floeter <git@reykfloeter.com>
2011-05-03 12:00:00 +0200

    You should never bet against anything in science at odds of more than about 10^12 to 1. -- Ernest Rutherford ... Added line 614 Id BpU8xIlzQWw= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -611,3 +611,4 @@
 #define MIB_pfLabelIndex		MIB_pfLabelEntry, 1
 #define MIB_pfLabelName			MIB_pfLabelEntry, 2
 #define MIB_pfLabelEvals		MIB_pfLabelEntry, 3
+#define MIB_pfLabelPkts			MIB_pfLabelEntry, 4
