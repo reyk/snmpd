e4224ab82dc63a3d04e953c31ae2da982cbf48b3
Reyk Floeter <git@reykfloeter.com>
2011-05-05 12:00:00 +0200

    A man said to the Universe: "Sir, I exist!" "However," replied the Universe, "the fact has not created in me a sense of obligation." -- Stephen Crane ... Added line 621 Id heKOVldV4p0= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -618,3 +618,4 @@
 #define MIB_pfLabelOutPkts		MIB_pfLabelEntry, 8
 #define MIB_pfLabelOutBytes		MIB_pfLabelEntry, 9
 #define MIB_pfLabelTotalStates		MIB_pfLabelEntry, 10
+#define MIB_pfsyncStats			MIB_pfMIBObjects, 11
