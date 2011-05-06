5cf3494ecbaf8f4000286fb525a0e18a39c41826
Reyk Floeter <git@reykfloeter.com>
2011-05-06 12:00:00 +0200

    A straw vote only shows which way the hot air blows. -- O'Henry ... Added line 626 Id 67P8fYOhfr8= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -623,3 +623,4 @@ snmpe_parse(struct sockaddr_storage *ss,
 	/* SNMP PDU */
 	if (ber_scanf_elements(a, "iiie{et",
 	    &req, &errval, &erridx, &msg->sm_pduend,
+	    &msg->sm_varbind, &class, &type) != 0) {
