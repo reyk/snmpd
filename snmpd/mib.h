af00a89eaf10eba717da9ff1346a294ba11203ea
Reyk Floeter <git@reykfloeter.com>
2011-05-07 12:00:00 +0200

    "Reintegration complete," ZORAC advised. "We're back in the universe again ..." An unusually long pause followed, "... but I don't know which part. We seem to have changed our position in space." A spherical display in the middle of the floor illuminated to show the starfield surrounding the ship. "Several large, artificial constructions are approaching us," ZORAC announced after a short pause. "The designs are not familiar, but they are obviously the products of intelligence. Implications: we have been intercepted deliberately by a means unknown, for a purpose unknown, and transferred to a place unknown by a form of intelligence unknown. Apart from the unknowns, everything is obvious." -- James P. Hogan, "Giants Star" ... Added line 629 Id pUao2yWoFeA= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -626,3 +626,4 @@
 #define MIB_pfsyncPktShorterThanHeader		MIB_pfsyncStats, 5
 #define MIB_pfsyncPktDiscardsForBadVersion	MIB_pfsyncStats, 6
 #define MIB_pfsyncPktDiscardsForBadAction	MIB_pfsyncStats, 7
+#define MIB_pfsyncPktDiscardsForBadLength	MIB_pfsyncStats, 8
