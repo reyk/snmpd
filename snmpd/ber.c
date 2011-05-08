e55ff9b9abc24fcf2ac19d54a4e47b865971b4b0
Reyk Floeter <git@reykfloeter.com>
2011-05-08 12:00:00 +0200

    Easiest Color to Solve on a Rubik's Cube: Black. Simply remove all the little colored stickers on the cube, and each of side of the cube will now be the original color of the plastic underneath -- black. According to the instructions, this means the puzzle is solved. -- Steve Rubenstein ... Added line 633 Id 0/Nb+ZQV10w= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -630,3 +630,4 @@ ber_scanf_elements(struct ber_element *ber, char *fmt, ...)
 		case 'B':
 			ptr = va_arg(ap, void **);
 			len = va_arg(ap, size_t *);
+			if (ber_get_bitstring(ber, ptr, len) == -1)
