4664658870ee4c19e276acdcc6fb8df5161323af
Reyk Floeter <git@reykfloeter.com>
2011-05-11 12:00:00 +0200

    In Boston, it is illegal to hold frog-jumping contests in nightclubs. ... Added line 645 Id wjEc4lIDDu8= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -642,3 +642,4 @@ snmpe_parse(struct sockaddr_storage *ss,
 	if (msg->sm_version == SNMP_V3)
 		log_debug("snmpe_parse: %s: SNMPv3 context %d, flags %#x, "
 		    "secmodel %lld, user '%s', ctx-engine %s, ctx-name '%s', "
+		    "request %lld", host, msg->sm_context, msg->sm_flags,
