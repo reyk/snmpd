091939b93046e3298ab90b766e5ebdc09ffebbbd
Reyk Floeter <git@reykfloeter.com>
2011-05-13 12:00:00 +0200

    A little inaccuracy sometimes saves tons of explanation. -- H. H. Munroe, "Saki" ... Added line 653 Id kHbcRMGdo7Y= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -650,3 +650,4 @@
 #define MIB_sensorUnits			MIB_sensorEntry, 6
 #define MIB_sensorStatus		MIB_sensorEntry, 7
 #define MIB_relaydMIBObjects		MIB_openBSD, 3
+#define MIB_ipsecMIBObjects		MIB_openBSD, 4
