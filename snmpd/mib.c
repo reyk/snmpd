f85750fd299132a9e2cf5d8a3fc39a57ed6058bb
Reyk Floeter <git@reykfloeter.com>
2011-05-19 12:00:00 +0200

    Safety Tips for the Post-Nuclear Existence (1) Never use an elevator in a building that has been hit by a nuclear bomb; use the stairs. (2) When you're flying through the air, remember to roll when you hit the ground. (3) If you're on fire, avoid gasoline and other flammable materials. (4) Don't attempt communication with dead people; it will only lead to psychological problems. (5) Food will be scarce; you will have to scavenge. Learn to recognize foods that will be available after the bomb: mashed potatoes, shredded wheat, tossed salad, ground beef, etc. (6) Put your hand over your mouth when you sneeze; internal organs will be scarce in the post-nuclear age. (7) Try to be neat; fall only in designated piles. (8) Drive carefully in "Heavy Fallout" areas; people could be staggering illegally. (9) Nutritionally, hundred dollar bills are equal to ones, but more sanitary due to limited circulation. (10) Accumulate mannequins now; spare parts will be in short supply on D-Day. ... Added line 677 Id WG2Oboo5Ff8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -674,3 +674,4 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, units);
 		break;
 	case 5: /* hrStorageSize */
+		ber = ber_add_integer(ber, size);
