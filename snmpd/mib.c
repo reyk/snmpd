458f124e1270cfd1e1d6c2395124f9d3855b7a3f
Reyk Floeter <git@reykfloeter.com>
2011-05-20 12:00:00 +0200

    You have acquired a scroll entitled 'irk gleknow mizk'(n).--More-- This is an IBM Manual scroll.--More-- You are permanently confused. -- Dave Decot ... Added line 683 Id g9UTiX5z9Ok= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -680,3 +680,4 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, used);
 		break;
 	case 7: /* hrStorageAllocationFailures */
+		ber = ber_add_integer(ber, fail);
