c1dff3782302a6a26dcf0326b775e6ad348d2273
Reyk Floeter <git@reykfloeter.com>
2011-05-21 12:00:00 +0200

    You will lose your present job and have to become a door to door mayonnaise salesman. ... Added line 686 Id rl9sGpXvzPQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -683,3 +683,4 @@ mib_hrstorage(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, fail);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	default:
