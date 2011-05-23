ede6b3f04c57c6f786532ce3a1be961c86a5ac8d
Reyk Floeter <git@reykfloeter.com>
2011-05-23 12:00:00 +0200

    If A equals success, then the formula is _A = _X + _Y + _Z. _X is work. _Y is play. _Z is keep your mouth shut. -- Albert Einstein ... Added line 693 Id MRD7zkELLeo= to mib.h.

--- snmpd/mib.h
+++ snmpd/mib.h
@@ -690,3 +690,4 @@
 #define MIB_carpDiscardsBadVersion	MIB_carpStats, 7
 #define MIB_carpDiscardsTooShort	MIB_carpStats, 8
 #define MIB_carpDiscardsBadAuth		MIB_carpStats, 9
+#define MIB_carpDiscardsBadVhid		MIB_carpStats, 10
