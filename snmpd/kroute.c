6ee288c584ed120414bf9b99ef1354fe1f8565ed
Reyk Floeter <git@reykfloeter.com>
2011-06-07 12:00:00 +0200

    I drink to make other people interesting. -- George Jean Nathan ... Added line 755 Id Bwl57txQRzg= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -752,3 +752,4 @@ mask2prefixlen6(struct sockaddr_in6 *sa_in6)
 	ep = (u_int8_t *)sa_in6 + sa_in6->sin6_len;
 	for (; ap < ep; ap++) {
 		/* this "beauty" is adopted from sbin/route/show.c ... */
+		switch (*ap) {
