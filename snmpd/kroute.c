745eefc6a4e3415791c8e513fe6890198493b2c9
Reyk Floeter <git@reykfloeter.com>
2011-06-07 12:00:00 +0200

    Warp 7 -- It's a law we can live with. ... Added line 752 Id iSvUc1BKBCQ= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -749,3 +749,4 @@ mask2prefixlen6(struct sockaddr_in6 *sa_in6)
 	 * the possibly truncated sin6_addr struct.
 	 */
 	ap = (u_int8_t *)&sa_in6->sin6_addr;
+	ep = (u_int8_t *)sa_in6 + sa_in6->sin6_len;
