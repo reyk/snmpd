b0f3a9929161eec10b3d08d114f87348170eb232
Reyk Floeter <git@reykfloeter.com>
2011-06-07 12:00:00 +0200

    Personifiers Unite! You have nothing to lose but Mr. Dignity! ... Added line 754 Id zjaQpxGir78= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -751,3 +751,4 @@ snmpe_parse(struct sockaddr_storage *ss,
  varfail:
 	log_debug("snmpe_parse: %s: %s, error index %d", host, errstr, i);
 	if (msg->sm_error == 0)
+		msg->sm_error = SNMP_ERROR_GENERR;
