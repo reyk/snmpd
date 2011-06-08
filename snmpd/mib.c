82cf0abbb883f5569a0de9abc943599e13ec942a
Reyk Floeter <git@reykfloeter.com>
2011-06-08 12:00:00 +0200

    I used to work in a fire hydrant factory. You couldn't park anywhere near the place. -- Steven Wright ... Added line 758 Id +PL05vnjvgQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -755,3 +755,4 @@ mib_hrprocessor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	int64_t			*cptime2, val;
 
 	/* Get and verify the current row index */
+	idx = o->bo_id[OIDIDX_hrDeviceEntry];
