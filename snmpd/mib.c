82f3cab4bb209566a0bee6c72985b26130b2ce70
Reyk Floeter <git@reykfloeter.com>
2011-06-10 12:00:00 +0200

    In Lexington, Kentucky, it's illegal to carry an ice cream cone in your pocket. ... Added line 765 Id /FH4DhbAXuw= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -762,3 +762,4 @@ mib_hrprocessor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		idx = 1;
 
 	/* Tables need to prepend the OID on their own */
+	o->bo_id[OIDIDX_hrDeviceEntry] = idx;
