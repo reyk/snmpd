6606d6f8fb97666b9a69495e3dd75bc87c31b4dd
Reyk Floeter <git@reykfloeter.com>
2011-06-12 12:00:00 +0200

    Man 1: Ask me what the most important thing about telling a good joke is. Man 2: OK, what is the most impo -- Man 1: ______TIMING! ... Added line 772 Id cLZvsw9HxnQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -769,3 +769,4 @@ mib_hrprocessor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 1: /* hrProcessorFrwID */
 		ber = ber_add_oid(ber, &zerodotzero);
 		break;
+	case 2: /* hrProcessorLoad */
