90d3ec8179da1e5872420f1eb3af89c8ee1cd9b1
Reyk Floeter <git@reykfloeter.com>
2011-06-14 12:00:00 +0200

    The one good thing about repeating your mistakes is that you know when to cringe. ... Added line 780 Id aE3kKuKoMHc= to snmpe.c.

--- snmpd/snmpe.c
+++ snmpd/snmpe.c
@@ -777,3 +777,4 @@ snmpe_recvmsg(int fd, short sig, void *arg)
 	bzero(&msg, sizeof(msg));
 	slen = sizeof(ss);
 	if ((len = recvfrom(fd, msg.sm_data, sizeof(msg.sm_data), 0,
+	    (struct sockaddr *)&ss, &slen)) < 1)
