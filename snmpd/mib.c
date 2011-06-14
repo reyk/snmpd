901faf640950195bf62937de057fb91d1deb8d2b
Reyk Floeter <git@reykfloeter.com>
2011-06-14 12:00:00 +0200

    Loan-department manager: "There isn't any fine print. At these interest rates, we don't need it." ... Added line 782 Id uD2roPehrdk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -779,3 +779,4 @@ mib_hrprocessor(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		cptime2 = env->sc_cpustates + (CPUSTATES * (idx - 1));
 		val = 100 -
 		    (cptime2[CP_IDLE] > 1000 ? 1000 : (cptime2[CP_IDLE] / 10));
+		ber = ber_add_integer(ber, val);
