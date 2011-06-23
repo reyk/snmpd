cc1691c159d38df8decfcfed0326b926b48211eb
Reyk Floeter <git@reykfloeter.com>
2011-06-23 12:00:00 +0200

    America was discovered by Amerigo Vespucci and was named after him, until people got tired of living in a place called "Vespuccia" and changed its name to "America". -- Mike Harding, "The Armchair Anarchist's Almanac" ... Added line 817 Id PduRbOjqXPM= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -814,3 +814,4 @@ get_rtaddrs(int addrs, struct sockaddr *sa, struct sockaddr **rti_info)
 	for (i = 0; i < RTAX_MAX; i++) {
 		if (addrs & (1 << i)) {
 			rti_info[i] = sa;
+			sa = (struct sockaddr *)((char *)(sa) +
