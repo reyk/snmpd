f9264c5532cd5fb6fd2e4d22743608be7c3dfeb4
Reyk Floeter <git@reykfloeter.com>
2011-06-26 12:00:00 +0200

    "Reflections on Ice-Breaking" Candy Is dandy But liquor Is quicker. -- Ogden Nash ... Added line 830 Id /HwhDvTOTtI= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -827,3 +827,4 @@ ber_free_elements(struct ber_element *root)
 	    root->be_encoding == BER_TYPE_SET))
 		ber_free_elements(root->be_sub);
 	if (root->be_next)
+		ber_free_elements(root->be_next);
