f9e2cf3f2b69e1a926e90a3f8afde6f411b847ed
Reyk Floeter <git@reykfloeter.com>
2011-06-27 12:00:00 +0200

    Boston, n.: Ludwig van Beethoven being jeered by 50,000 sports fans for finishing second in the Irish jig competition. ... Added line 832 Id 249AsSS3/ow= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -829,3 +829,4 @@ mib_hrswrun(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			ber = ber_add_integer(ber, 2);
 		} else {
 			/* application(4) */
+			ber = ber_add_integer(ber, 4);
