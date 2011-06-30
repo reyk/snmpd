f43bb845d638791847815ffe1ced966f38a17db8
Reyk Floeter <git@reykfloeter.com>
2011-06-30 12:00:00 +0200

    Always remember that you are unique. Just like everyone else. ... Added line 847 Id TLhEShrqkoc= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -844,3 +844,4 @@ ber_calc_len(struct ber_element *root)
 
 	/* calculate the real length of a sequence or set */
 	if (root->be_sub && (root->be_encoding == BER_TYPE_SEQUENCE ||
+	    root->be_encoding == BER_TYPE_SET))
