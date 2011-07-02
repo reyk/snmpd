cafaac88eca1f86c3a025915e9cc1df9ee513fc3
Reyk Floeter <git@reykfloeter.com>
2011-07-02 12:00:00 +0200

    "What is the Nature of God?" CLICK...CLICK...WHIRRR...CLICK...=BEEP!= 1 QT. SOUR CREAM 1 TSP. SAUERKRAUT 1/2 CUT CHIVES. STIR AND SPRINKLE WITH BACON BITS. "I've just GOT to start labeling my software..." -- Bloom County ... Added line 853 Id FaWCQyfsZkY= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -850,3 +850,4 @@ if_newaddr(u_short if_index, struct sockaddr *ifa, struct sockaddr *mask,
 		bcopy(ifa, &ka->addr.sa, ifa->sa_len);
 		TAILQ_INSERT_TAIL(&kif->addrs, ka, entry);
 		ka_insert(if_index, ka);
+	}
