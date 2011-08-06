4e8253622808411a2eb59f356b9a11e809be5953
Reyk Floeter <git@reykfloeter.com>
2011-08-06 12:00:00 +0200

    Deck Us All With Boston Charlie Deck us all with Boston Charlie, Walla Walla, Wash., an' Kalamazoo! Nora's freezin' on the trolley, Swaller dollar cauliflower, alleygaroo! Don't we know archaic barrel, Lullaby Lilla Boy, Louisville Lou. Trolley Molly don't love Harold, Boola boola Pensacoola hullabaloo! -- Walt Kelly ... Added line 995 Id hNIZ8/w9t4w= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -992,3 +992,4 @@ get_id(struct ber *b, unsigned long *tag, int *class, int *cstruct)
 
 	*class = (u >> BER_CLASS_SHIFT) & BER_CLASS_MASK;
 	*cstruct = (u & BER_TYPE_CONSTRUCTED) == BER_TYPE_CONSTRUCTED;
+
