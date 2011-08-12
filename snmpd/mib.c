b7fadf4b0bdafb1cd69bb2165f7828386b270ff5
Reyk Floeter <git@reykfloeter.com>
2011-08-12 12:00:00 +0200

    Of all the words of witch's doom There's none so bad as which and whom. The man who kills both which and whom Will be enshrined in our Who's Whom. -- Fletcher Knebel ... Added line 1018 Id sGgNktaHTrQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1015,3 +1015,4 @@ static struct oid if_mib[] = {
 	{ MIB(ifRcvAddressStatus),	OID_TRD, mib_ifrcvtable },
 	{ MIB(ifRcvAddressType),	OID_TRD, mib_ifrcvtable },
 	{ MIB(ifStackLastChange),	OID_RD, mib_ifstacklast },
+	{ MIB(ifNumber),		OID_RD, mib_ifnumber },
