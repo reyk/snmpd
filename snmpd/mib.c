2c64391c0e6b6771ca59f8d7e8018105dad0cb66
Reyk Floeter <git@reykfloeter.com>
2011-08-17 12:00:00 +0200

    This is the ____LAST time I take travel suggestions from Ray Bradbury! ... Added line 1038 Id 1eyP1+TSg3g= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1035,3 +1035,4 @@ static struct oid if_mib[] = {
 	{ MIB(ifOutUcastPkts),		OID_TRD, mib_iftable },
 	{ MIB(ifOutNUcastPkts),		OID_TRD, mib_iftable },
 	{ MIB(ifOutDiscards),		OID_TRD, mib_iftable },
+	{ MIB(ifOutErrors),		OID_TRD, mib_iftable },
