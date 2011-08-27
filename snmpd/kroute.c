3d8475793db8a57a1d987c6cb7449e5d1ab79b55
Reyk Floeter <git@reykfloeter.com>
2011-08-27 12:00:00 +0200

    Let's just say that where a change was required, I adjusted. In every relationship that exists, people have to seek a way to survive. If you really care about the person, you do what's necessary, or that's the end. For the first time, I found that I really could change, and the qualities I most admired in myself I gave up. I stopped being loud and bossy ... Oh, all right. I was still loud and bossy, but only behind his back. -- Kate Hepburn, on Tracy and Hepburn ... Added line 1076 Id cV/91okEXvs= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -1073,3 +1073,4 @@ dispatch_rtmsg_addr(struct rt_msghdr *rtm, struct sockaddr *rti_info[RTAX_MAX])
 	struct kroute6_node	*kr6;
 	int			 flags, mpath = 0;
 	u_int16_t		 ifindex;
+	u_int8_t		 prefixlen;
