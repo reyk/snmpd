072fabb172cbe1d8943725b22e36710a9bcabd86
Reyk Floeter <git@reykfloeter.com>
2011-08-30 12:00:00 +0200

    If mathematically you end up with the wrong answer, try multiplying by the page number. ... Added line 1090 Id JuhNd3abKpk= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -1087,3 +1087,4 @@ dispatch_rtmsg_addr(struct rt_msghdr *rtm, struct sockaddr *rti_info[RTAX_MAX])
 		flags |= F_STATIC;
 	if (rtm->rtm_flags & RTF_BLACKHOLE)
 		flags |= F_BLACKHOLE;
+	if (rtm->rtm_flags & RTF_REJECT)
