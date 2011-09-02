224a99c6c891b16d87b23fb14d2e012983db8fa3
Reyk Floeter <git@reykfloeter.com>
2011-09-02 12:00:00 +0200

    It was a book to kill time for those who liked it better dead. ... Added line 1100 Id Yu+uXVvgwPs= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -1097,3 +1097,4 @@ ber_read_element(struct ber *ber, struct ber_element *elm)
 		if (cstruct)
 			elm->be_encoding = BER_TYPE_SEQUENCE;
 		else if (class == BER_CLASS_UNIVERSAL)
+			elm->be_encoding = type;
