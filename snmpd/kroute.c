8c1898ad4a16a5abf7d5262a421f7f302e047c0d
Reyk Floeter <git@reykfloeter.com>
2011-09-03 12:00:00 +0200

    There can be no twisted thought without a twisted molecule. -- R. W. Gerard ... Added line 1106 Id HTIOcdOnt0k= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -1103,3 +1103,4 @@ dispatch_rtmsg_addr(struct rt_msghdr *rtm, struct sockaddr *rti_info[RTAX_MAX])
 		sa_in = (struct sockaddr_in *)rti_info[RTAX_NETMASK];
 		if (sa_in != NULL) {
 			if (sa_in->sin_len != 0)
+				prefixlen = mask2prefixlen(
