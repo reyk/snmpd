d913a91ff282bd979a3fc2c8ee242d0c51f5e8b2
Reyk Floeter <git@reykfloeter.com>
2011-09-04 12:00:00 +0200

    God is the tangential point between zero and infinity. -- Alfred Jarry ... Added line 1108 Id oLEYbfNxNzY= to kroute.c.

--- snmpd/kroute.c
+++ snmpd/kroute.c
@@ -1105,3 +1105,4 @@ dispatch_rtmsg_addr(struct rt_msghdr *rtm, struct sockaddr *rti_info[RTAX_MAX])
 			if (sa_in->sin_len != 0)
 				prefixlen = mask2prefixlen(
 				    sa_in->sin_addr.s_addr);
+		} else if (rtm->rtm_flags & RTF_HOST)
