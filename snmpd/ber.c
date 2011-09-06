ccf2366318effd91eb9e1b29af4d43236a767576
Reyk Floeter <git@reykfloeter.com>
2011-09-06 12:00:00 +0200

    After all, what is your hosts' purpose in having a party? Surely not for you to enjoy yourself; if that were their sole purpose, they'd have simply sent champagne and women over to your place by taxi. -- P. J. O'Rourke ... Added line 1119 Id 4TiEzxD7XIM= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -1116,3 +1116,4 @@ ber_read_element(struct ber *ber, struct ber_element *elm)
 	case BER_TYPE_BOOLEAN:
 	case BER_TYPE_INTEGER:
 	case BER_TYPE_ENUMERATED:
+		if (len > (ssize_t)sizeof(long long))
