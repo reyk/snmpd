016f3f8af518fac1572ac19de27c0b4a29b64679
Reyk Floeter <git@reykfloeter.com>
2011-09-09 12:00:00 +0200

    If you stand on your head, you will get footprints in your hair. ... Added line 1129 Id uH0bw3Mq5mg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1126,3 +1126,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 5:
 		ber = ber_add_integer(ber, kif->if_baudrate);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
+		break;
