bb4ff25eea33ccff9804017e1244b4e2f5a4bc20
Reyk Floeter <git@reykfloeter.com>
2011-09-11 12:00:00 +0200

    If God had meant for us to be naked, we would have been born that way. ... Added line 1136 Id NxmXIQ7XIu8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1133,3 +1133,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			ber = ber_add_string(ber, "");
 		} else {
 			ber = ber_add_nstring(ber, kif->if_lladdr,
+			    sizeof(kif->if_lladdr));
