06f9634fcbfd10b7b64ea5dc84bfd4d0c199370c
Reyk Floeter <git@reykfloeter.com>
2011-09-12 12:00:00 +0200

    I went to a job interview the other day, the guy asked me if I had any questions, I said yes, just one, if you're in a car traveling at the speed of light and you turn your headlights on, does anything happen? He said he couldn't answer that, I told him sorry, but I couldn't work for him then. -- Steven Wright ... Added line 1142 Id FcdZ1Ww7wGk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1139,3 +1139,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 7:
 		/* ifAdminStatus up(1), down(2), testing(3) */
 		i = (kif->if_flags & IFF_UP) ? 1 : 2;
+		ber = ber_add_integer(ber, i);
