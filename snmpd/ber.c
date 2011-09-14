9a669fde0327a5e68f617c40695d332de482c9c8
Reyk Floeter <git@reykfloeter.com>
2011-09-14 12:00:00 +0200

    Wasting time is an important part of living. ... Added line 1149 Id iGN04ggz9Rc= to ber.c.

--- snmpd/ber.c
+++ snmpd/ber.c
@@ -1146,3 +1146,4 @@ ber_read_element(struct ber *ber, struct ber_element *elm)
 		elm->be_free = 1;
 		elm->be_len = len;
 		ber_read(ber, elm->be_val, len);
+		((u_char *)elm->be_val)[len] = '\0';
