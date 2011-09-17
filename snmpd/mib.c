d5f1c35ceeb9be8363e6ada7923b3b3ef175f246
Reyk Floeter <git@reykfloeter.com>
2011-09-17 12:00:00 +0200

    The more laws and order are made prominent, the more thieves and robbers there will be. -- Lao Tsu ... Added line 1162 Id SgjoSDIupt8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1159,3 +1159,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 10:
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_ibytes);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
