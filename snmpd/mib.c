75a15b67ecf63e612b6280c71e5670b220fc9906
Reyk Floeter <git@reykfloeter.com>
2011-09-19 12:00:00 +0200

    The fortune program is supported, in part, by user contributions and by a major grant from the National Endowment for the Inanities. ... Added line 1168 Id 0mOUNJmdLBI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1165,3 +1165,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_ipackets);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	case 12:
