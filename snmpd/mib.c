71f75d5745b1fb62aef74fdf8f944b09e5dda188
Reyk Floeter <git@reykfloeter.com>
2011-09-19 12:00:00 +0200

    Captain Penny's Law: You can fool all of the people some of the time, and some of the people all of the time, but you Can't Fool Mom. ... Added line 1170 Id D0WshkkeZTY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1167,3 +1167,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 12:
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_imcasts);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
