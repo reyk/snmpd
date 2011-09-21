1fb152eb847ccbcc88517ee9212cb10943735776
Reyk Floeter <git@reykfloeter.com>
2011-09-21 12:00:00 +0200

    Health is merely the slowest possible rate at which one can die. ... Added line 1177 Id S5qZxEwAXoA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1174,3 +1174,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		mib[4] = IFQCTL_DROPS;
 		len = sizeof(ifq);
 		if (sysctl(mib, sizeofa(mib), &ifq, &len, 0, 0) == -1) {
+			log_info("mib_iftable: %s: invalid ifq: %s",
