4e914d093d2dbf44c039e773298e9d9729b16b81
Reyk Floeter <git@reykfloeter.com>
2011-09-24 12:00:00 +0200

    People will do tomorrow what they did today because that is what they did yesterday. ... Added line 1190 Id 0FnurG6Puhc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1187,3 +1187,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 15:
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_noproto);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
