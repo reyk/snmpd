f6ca5b3b23dbed0648c3811664a31548ac898424
Reyk Floeter <git@reykfloeter.com>
2011-09-24 12:00:00 +0200

    I do hate sums. There is no greater mistake than to call arithmetic an exact science. There are permutations and aberrations discernible to minds entirely noble like mine; subtle variations which ordinary accountants fail to discover; hidden laws of number which it requires a mind like mine to perceive. For instance, if you add a sum from the bottom up, and then again from the top down, the result is always different. -- Mrs. La Touche (19th cent.) ... Added line 1189 Id lJLpmz5GbbQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1186,3 +1186,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 15:
+		ber = ber_add_integer(ber, (u_int32_t)kif->if_noproto);
