0d07234fdbb3a03686324b380d11bd66a1654f34
Reyk Floeter <git@reykfloeter.com>
2011-09-25 12:00:00 +0200

    If I don't see you in the future, I'll see you in the pasture. ... Added line 1195 Id OrgEDNB7LGQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1192,3 +1192,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 16:
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_obytes);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
+		break;
