5065f09b1114c289410c1577e56c9b0bb937f2b1
Reyk Floeter <git@reykfloeter.com>
2011-09-27 12:00:00 +0200

    Ladybug, ladybug, Look to your stern! Your house is on fire, Your children will burn! So jump ye and sing, for The very first time The four lines above Have been put into rhyme. -- Walt Kelly ... Added line 1201 Id AnPopjJKYZs= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1198,3 +1198,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 18:
+		ber = ber_add_integer(ber, (u_int32_t)kif->if_omcasts);
