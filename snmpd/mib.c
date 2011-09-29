68940a6a20aaa761c2de8558d9b280f6779336a4
Reyk Floeter <git@reykfloeter.com>
2011-09-29 12:00:00 +0200

    Surprise due today. Also the rent. ... Added line 1210 Id I9nZwNhAR9o= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1207,3 +1207,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 20:
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_oerrors);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
