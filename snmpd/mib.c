6f209e8b887dcfe126d30c5db0111a11b2a1a9d2
Reyk Floeter <git@reykfloeter.com>
2011-09-30 12:00:00 +0200

    What I do, first thing [in the morning], is I hop into the shower stall. Then I hop right back out, because when I hopped in I landed barefoot right on top of See Threepio, a little plastic robot character from "Star Wars" whom my son, Robert, likes to pull the legs off of while he showers. Then I hop right back into the stall because our dog, Earnest, who has been alone in the basement all night building up powerful dog emotions, has come bounding and quivering into the bathroom and wants to greet me with 60 or 70 thousand playful nips, any one of which -- bear in mind that I am naked and, without my contact lenses, essentially blind -- could result in the kind of injury where you have to learn a whole new part if you want to sing the "Messiah", if you get my drift. Then I hop right back out, because Robert, with that uncanny sixth sense some children have -- you cannot teach it; they either have it or they don't -- has chosen exactly that moment to flush one of the toilets. Perhaps several of them. -- Dave Barry, "Saving Face" ... Added line 1212 Id Y5YN4rJJP9k= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1209,3 +1209,4 @@ mib_iftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_oerrors);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	case 21:
