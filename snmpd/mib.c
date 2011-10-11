a4b6bbddc2dc0f89e7e70f298d73ce92e5db7290
Reyk Floeter <git@reykfloeter.com>
2011-10-11 12:00:00 +0200

    The church is near but the road is icy; the bar is far away but I will walk carefully. -- Russian Proverb ... Added line 1259 Id CqV2D5jRUrA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1256,3 +1256,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, (u_int32_t)kif->if_imcasts);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	case 3:
