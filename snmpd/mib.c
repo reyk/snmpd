2397ca975bbc68ac7fb68a0a1eab226edfe6fc2b
Reyk Floeter <git@reykfloeter.com>
2011-10-13 12:00:00 +0200

    Hardware, n.: The parts of a computer system that can be kicked. ... Added line 1264 Id R9ZXTb7YKOE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1261,3 +1261,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 4:
+		ber = ber_add_integer(ber, (u_int32_t)kif->if_omcasts);
