90432b31b4fd334b70d556c59e75f0eb798f62f4
Reyk Floeter <git@reykfloeter.com>
2011-10-14 12:00:00 +0200

    The fact that it works is immaterial. -- L. Ogborn ... Added line 1270 Id C0s2sFbV2LM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1267,3 +1267,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 5:
 		ber = ber_add_integer(ber, 0);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
+		break;
