e9beb6e93a904856fd43e67df0a1289c41baf032
Reyk Floeter <git@reykfloeter.com>
2011-10-14 12:00:00 +0200

    ... I'm IMAGINING a sensuous GIRAFFE, CAVORTING in the BACK ROOM of a KOSHER DELI!! ... Added line 1268 Id iVqzzgATm1U= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1265,3 +1265,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 5:
+		ber = ber_add_integer(ber, 0);
