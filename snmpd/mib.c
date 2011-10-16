1d2e60751affe2458e25606bd0edaba828b85413
Reyk Floeter <git@reykfloeter.com>
2011-10-16 12:00:00 +0200

    When I get real bored, I like to drive downtown and get a great parking spot, then sit in my car and count how many people ask me if I'm leaving. -- Steven Wright ... Added line 1278 Id /SqTcfgaa5Y= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1275,3 +1275,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 7:
 		ber = ber_add_integer(ber, (u_int64_t)kif->if_ipackets);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
