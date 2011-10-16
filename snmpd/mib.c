93b9aff4db8f5b2feb1d67d3466dbae94dc5c750
Reyk Floeter <git@reykfloeter.com>
2011-10-16 12:00:00 +0200

    Ninety percent of the time things turn out worse than you thought they would. The other ten percent of the time you had no right to expect that much. -- Augustine ... Added line 1277 Id xK6vFziGtB0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1274,3 +1274,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 7:
 		ber = ber_add_integer(ber, (u_int64_t)kif->if_ipackets);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
