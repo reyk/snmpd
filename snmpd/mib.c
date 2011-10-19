dbd75f007d1678fae8aff06998463953124644fe
Reyk Floeter <git@reykfloeter.com>
2011-10-19 12:00:00 +0200

    Good-bye. I am leaving because I am bored. -- George Saunders' dying words ... Added line 1289 Id cL62YAQDUVw= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1286,3 +1286,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 10:
 		ber = ber_add_integer(ber, (u_int64_t)kif->if_obytes);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
