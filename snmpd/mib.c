d414ef6347754510e40d9e93c0ef48050689b8cd
Reyk Floeter <git@reykfloeter.com>
2011-10-19 12:00:00 +0200

    Men were real men, women were real women, and small, furry creatures from Alpha Centauri were REAL small, furry creatures from Alpha Centauri. Spirits were brave, men boldly split infinitives that no man had split before. Thus was the Empire forged. -- "The Hitchhiker's Guide to the Galaxy", Douglas Adams ... Added line 1290 Id nymgT+zno5U= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1287,3 +1287,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 10:
 		ber = ber_add_integer(ber, (u_int64_t)kif->if_obytes);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
