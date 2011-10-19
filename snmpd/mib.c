9f9f52e929b14fd99d5a8bf0541d646d3906ce93
Reyk Floeter <git@reykfloeter.com>
2011-10-19 12:00:00 +0200

    The moving cursor writes, and having written, blinks on. ... Added line 1288 Id dLHDzYUbC9U= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1285,3 +1285,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 10:
+		ber = ber_add_integer(ber, (u_int64_t)kif->if_obytes);
