441d18d5955bd4c1abf4fd928bf6f805faf4c42d
Reyk Floeter <git@reykfloeter.com>
2011-10-21 12:00:00 +0200

    Picture the sun as the origin of two intersecting 6-dimensional hyperplanes from which we can deduce a certain transformational sequence which gives us the terminal velocity of a rubber duck ... ... Added line 1296 Id EHyTOMwRLKI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1293,3 +1293,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 12:
+		ber = ber_add_integer(ber, (u_int64_t)kif->if_omcasts);
