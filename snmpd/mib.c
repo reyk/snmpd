1f1f4ebf9baab3a83e96ed03daebff877b1d2a3b
Reyk Floeter <git@reykfloeter.com>
2011-10-22 12:00:00 +0200

    The mome rath isn't born that could outgrabe me. -- Nicol Williamson ... Added line 1300 Id FvXLPyDaJNw= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1297,3 +1297,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 13:
+		ber = ber_add_integer(ber, 0);
