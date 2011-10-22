c56d2b874de3f15fc13509b72a18109914575be0
Reyk Floeter <git@reykfloeter.com>
2011-10-22 12:00:00 +0200

    Peace, n.: In international affairs, a period of cheating between two periods of fighting. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 1303 Id /Ddv0XUpcmc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1300,3 +1300,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, 0);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 14:
