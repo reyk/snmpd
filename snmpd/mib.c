ea3c49aebb7106db71baa117bc513916e0ca213d
Reyk Floeter <git@reykfloeter.com>
2011-10-24 12:00:00 +0200

    Gosh that takes me back... or is it forward? That's the trouble with time travel, you never can tell." -- Doctor Who "Androids of Tara" ... Added line 1310 Id w81FHk67IDc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1307,3 +1307,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		i = kif->if_baudrate >= 1000000 ?
 		    kif->if_baudrate / 1000000 : 0;
 		ber = ber_add_integer(ber, i);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
