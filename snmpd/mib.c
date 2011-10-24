9f87af2651dade3d4cfab94e90170cfb4b5f63bc
Reyk Floeter <git@reykfloeter.com>
2011-10-24 12:00:00 +0200

    Why did the Lord give us so much quickness of movement unless it was to avoid responsibility with? ... Added line 1309 Id hdLA3Z6Zsn8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1306,3 +1306,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 15:
 		i = kif->if_baudrate >= 1000000 ?
 		    kif->if_baudrate / 1000000 : 0;
+		ber = ber_add_integer(ber, i);
