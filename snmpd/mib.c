f3c28d0d536410dd4bafe11c1feaaa880bd38c4d
Reyk Floeter <git@reykfloeter.com>
2011-10-28 12:00:00 +0200

    The Advertising Agency Song: When your client's hopping mad, Put his picture in the ad. If he still should prove refractory, Add a picture of his factory. ... Added line 1327 Id nqRADd+vyKw= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1324,3 +1324,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 19:
 		ber = ber_add_integer(ber, 0);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
