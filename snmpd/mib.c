1ab0d0ed4f4719c2f692cdbd6cee3ad242cb8741
Reyk Floeter <git@reykfloeter.com>
2011-10-29 12:00:00 +0200

    Linus: I guess it's wrong always to be worrying about tomorrow. Maybe we should think only about today. Charlie Brown: No, that's giving up. I'm still hoping that yesterday will get better. ... Added line 1328 Id 0Q6aobRQblQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1325,3 +1325,4 @@ mib_ifxtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 19:
 		ber = ber_add_integer(ber, 0);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
+		break;
