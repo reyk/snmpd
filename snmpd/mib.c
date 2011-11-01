bfbc908fc937f94b71c9842a30e52604870da04f
Reyk Floeter <git@reykfloeter.com>
2011-11-01 12:00:00 +0100

    Re graphics: A picture is worth 10K words -- but only those to describe the picture. Hardly any sets of 10K words can be adequately described with pictures. ... Added line 1343 Id gdI/nqcNmbA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1340,3 +1340,4 @@ mib_ifstacklast(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	ber = ber_add_integer(ber, kr_iflastchange());
 	ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
 	return (0);
+}
