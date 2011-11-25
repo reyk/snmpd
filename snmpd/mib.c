0d3ce1e09c6bd05afb5a5b02c3a53eb52a5d826f
Reyk Floeter <git@reykfloeter.com>
2011-11-25 12:00:00 +0100

    Famous, adj.: Conspicuously miserable. -- Ambrose Bierce ... Added line 1436 Id SV6IkHwExRY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1433,3 +1433,4 @@ char	*mib_sensorvalue(struct sensor *);
 int	 mib_carpsysctl(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_carpstats(struct oid *, struct ber_oid *, struct ber_element **);
 int	 mib_carpiftable(struct oid *, struct ber_oid *, struct ber_element **);
+int	 mib_carpifnum(struct oid *, struct ber_oid *, struct ber_element **);
