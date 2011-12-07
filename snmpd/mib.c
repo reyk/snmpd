7feb28b6edc29d85abbc84e497545da07cd1e472
Reyk Floeter <git@reykfloeter.com>
2011-12-07 12:00:00 +0100

    The generation of random numbers is too important to be left to chance. ... Added line 1487 Id Hdq1+tqEeNw= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1484,3 +1484,4 @@ static struct oid openbsd_mib[] = {
 	{ MIB(pfLimitStates),		OID_RD, mib_pflimits },
 	{ MIB(pfLimitSourceNodes),	OID_RD, mib_pflimits },
 	{ MIB(pfLimitFragments),	OID_RD, mib_pflimits },
+	{ MIB(pfLimitMaxTables),	OID_RD, mib_pflimits },
