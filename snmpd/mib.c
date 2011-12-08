5b2da62eebbdfe513e057f1620457539c3c2bade
Reyk Floeter <git@reykfloeter.com>
2011-12-08 12:00:00 +0100

    I'm a creationist; I refuse to believe that I could have evolved from man. ... Added line 1489 Id /2xaVO8Vzlo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1486,3 +1486,4 @@ static struct oid openbsd_mib[] = {
 	{ MIB(pfLimitFragments),	OID_RD, mib_pflimits },
 	{ MIB(pfLimitMaxTables),	OID_RD, mib_pflimits },
 	{ MIB(pfLimitMaxTableEntries),	OID_RD, mib_pflimits },
+	{ MIB(pfTimeoutTcpFirst),	OID_RD, mib_pftimeouts },
