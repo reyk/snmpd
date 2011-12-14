1ff1a8ca767e4ddf244e9ecd188cce952832c2a4
Reyk Floeter <git@reykfloeter.com>
2011-12-14 12:00:00 +0100

    The notion that the church, the press, and the universities should serve the state is essentially a Communist notion ... In a free society these institutions must be wholly free -- which is to say that their function is to serve as checks upon the state. -- Alan Barth ... Added line 1515 Id ftXj2FFMJBY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1512,3 +1512,4 @@ static struct oid openbsd_mib[] = {
 	{ MIB(pfIfRefs),		OID_TRD, mib_pfiftable },
 	{ MIB(pfIfRules),		OID_TRD, mib_pfiftable },
 	{ MIB(pfIfIn4PassPkts),		OID_TRD, mib_pfiftable },
+	{ MIB(pfIfIn4PassBytes),	OID_TRD, mib_pfiftable },
