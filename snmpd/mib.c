8d04b20998a2481c98f0c9ca2c7f47ba338cb996
Reyk Floeter <git@reykfloeter.com>
2011-12-26 12:00:00 +0100

    If entropy is increasing, where is it coming from? ... Added line 1563 Id 6NOAm+V1aI4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1560,3 +1560,4 @@ static struct oid openbsd_mib[] = {
 	    NULL, mib_pftableaddrstable },
 	{ MIB(pfTblAddrInBlockBytes),	OID_TRD, mib_pftableaddrs,
 	    NULL, mib_pftableaddrstable },
+	{ MIB(pfTblAddrInPassPkts),	OID_TRD, mib_pftableaddrs,
