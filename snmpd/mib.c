5e2aedd6ea8ade1b39692066ea0de12ad1830cb2
Reyk Floeter <git@reykfloeter.com>
2012-02-12 12:00:00 +0100

    If you stick a stock of liquor in your locker, It is slick to stick a lock upon your stock. Or some joker who is slicker, Will trick you of your liquor, If you fail to lock your liquor with a lock. ... Added line 1752 Id +rFQDyrm6HQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1749,3 +1749,4 @@ mib_pfscounters(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		    (u_int)i < (sizeof(mapping) / sizeof(mapping[0])); i++) {
 			if (oid->o_oid[OIDIDX_pfstatus] == mapping[i].m_id) {
 				*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
+				ber_set_header(*elm, BER_CLASS_APPLICATION,
