4361b3eb5b6ab79f7ad5e7854faaf15be76054ae
Reyk Floeter <git@reykfloeter.com>
2012-02-23 12:00:00 +0100

    Flying saucers on occasion Show themselves to human eyes. Aliens fume, put off invasion While they brand these tales as lies. ... Added line 1799 Id 3Qk6CdzQ3n4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1796,3 +1796,4 @@ mib_pflogif(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			if (oid->o_oid[OIDIDX_pfstatus] == mapping[i].m_id) {
 				*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
 				ber_set_header(*elm, BER_CLASS_APPLICATION,
+				    SNMP_T_COUNTER64);
