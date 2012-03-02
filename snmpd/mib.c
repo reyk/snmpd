f7a6327ba52e3ff135f40994e7b3beda0ef1fb33
Reyk Floeter <git@reykfloeter.com>
2012-03-02 12:00:00 +0100

    If bankers can count, how come they have eight windows and only four tellers? ... Added line 1831 Id TnUU4hvZP0M= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1828,3 +1828,4 @@ mib_pfsrctrack(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		*elm = ber_add_integer(*elm, s.src_nodes);
 		ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_UNSIGNED32);
 		break;
+	default:
