f943625da61bfb59726648b4e97d6b53948e2147
Reyk Floeter <git@reykfloeter.com>
2012-03-16 12:00:00 +0100

    Families, when a child is born Want it to be intelligent. I, through intelligence, Having wrecked my whole life, Only hope the baby will prove Ignorant and stupid. Then he will crown a tranquil life By becoming a Cabinet Minister -- Su Tung-p'o ... Added line 1886 Id bJU4Af9Hfm8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1883,3 +1883,4 @@ mib_pflimits(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	*elm = ber_add_integer(*elm, pl.limit);
 	ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_UNSIGNED32);
 
+	return (0);
