97f1293a80980e9d52cd131842d785b41c7ec182
Reyk Floeter <git@reykfloeter.com>
2012-03-26 12:00:00 +0200

    The bureaucracy is expanding to meet the needs of an expanding bureaucracy. ... Added line 1925 Id dT6iglvP+4I= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1922,3 +1922,4 @@ mib_pftimeouts(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 
 	for (i = 0;
 	    (u_int)i < (sizeof(mapping) / sizeof(mapping[0])); i++) {
+		if (oid->o_oid[OIDIDX_pfstatus] == mapping[i].m_id) {
