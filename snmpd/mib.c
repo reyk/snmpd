3d9446725e25817c017210c62046679a60a9d994
Reyk Floeter <git@reykfloeter.com>
2012-04-10 12:00:00 +0200

    Majority, n.: That quality that distinguishes a crime from a law. ... Added line 1987 Id K19iRSHHN8g= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1984,3 +1984,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 4:
 		ber = ber_add_integer(ber, pif.pfik_states);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_UNSIGNED32);
+		break;
