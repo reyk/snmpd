82d67ddf487d80de11c39b976c6689a6ae707372
Reyk Floeter <git@reykfloeter.com>
2012-04-13 12:00:00 +0200

    On Monday mornings I am dedicated to the proposition that all men are created jerks. -- Avery ... Added line 1998 Id jMb06kIII1I= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1995,3 +1995,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 7:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][IN][PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
