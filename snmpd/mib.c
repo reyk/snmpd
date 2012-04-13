bd615a9d8aea815f24e6ed2053b1a29b2a85e911
Reyk Floeter <git@reykfloeter.com>
2012-04-13 12:00:00 +0200

    A bird in the hand is worth what it will bring. ... Added line 1999 Id 6CC/L5qY9Fc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -1996,3 +1996,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 7:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][IN][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
