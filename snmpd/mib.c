11fea94eadff9baa1e685efab77fa9688b01193c
Reyk Floeter <git@reykfloeter.com>
2012-04-15 12:00:00 +0200

    Painting, n.: The art of protecting flat surfaces from the weather, and exposing them to the critic. -- Ambrose Bierce ... Added line 2006 Id PWIuWdAbQxA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2003,3 +2003,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 9:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][IN][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
