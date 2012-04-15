0c61e9c933e27d7a4affbe9afaebbcab20540d99
Reyk Floeter <git@reykfloeter.com>
2012-04-15 12:00:00 +0200

    T: One big monster, he called TROLL. He don't rock, and he don't roll; Drink no wine, and smoke no stogies. He just Love To Eat Them Roguies. -- The Roguelet's ABC ... Added line 2005 Id r+pEZhZ4pZY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2002,3 +2002,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 9:
+		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][IN][BLOCK]);
