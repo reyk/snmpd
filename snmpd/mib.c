7a7684313568d0feeca8dd29f2285ad10f291af7
Reyk Floeter <git@reykfloeter.com>
2012-04-16 12:00:00 +0200

    Think of it! With VLSI we can pack 100 ENIACs in 1 sq. cm.! ... Added line 2010 Id Q5qnMp1alF4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2007,3 +2007,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 10:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV4][OUT][PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
