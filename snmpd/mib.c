fa10f914520ceb78f4c8bfb7c1d45541d34ab59a
Reyk Floeter <git@reykfloeter.com>
2012-04-16 12:00:00 +0200

    A lot of people are afraid of heights. Not me. I'm afraid of widths. -- Steve Wright ... Added line 2009 Id JLLWNxyZ+9o= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2006,3 +2006,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 10:
+		ber = ber_add_integer(ber, pif.pfik_packets[IPV4][OUT][PASS]);
