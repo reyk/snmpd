5747d4c66ceec7266dee4031b563021da931f53d
Reyk Floeter <git@reykfloeter.com>
2012-04-17 12:00:00 +0200

    10.0 times 0.1 is hardly ever 1.0. ... Added line 2015 Id 8UndWDzuO1c= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2012,3 +2012,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 11:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][OUT][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
