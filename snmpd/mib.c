6526b7bccfd70fe7b3437aef9c242300e7fa1fc0
Reyk Floeter <git@reykfloeter.com>
2012-04-17 12:00:00 +0200

    Ask not for whom the telephone bell tolls ... if thou art in the bathtub, it tolls for thee. ... Added line 2014 Id 1O3qk4TxxTY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2011,3 +2011,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 11:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][OUT][PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
