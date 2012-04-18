a59a553a199fa98bc5fafe26c03b4b125323f251
Reyk Floeter <git@reykfloeter.com>
2012-04-18 12:00:00 +0200

    Philosophy will clip an angel's wings. -- John Keats ... Added line 2016 Id RWfGMVc0CL4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2013,3 +2013,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][OUT][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 12:
