6ef34b1f9a9d27ef6855c2a8cb9e5292c3d7c756
Reyk Floeter <git@reykfloeter.com>
2012-04-19 12:00:00 +0200

    "A University without students is like an ointment without a fly." -- Ed Nather, professor of astronomy at UT Austin ... Added line 2022 Id 64VA9Qhfgis= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2019,3 +2019,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 13:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][OUT][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
