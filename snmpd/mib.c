71f8717145a5f94bd1e10e3aeb04187ceb4eb3f0
Reyk Floeter <git@reykfloeter.com>
2012-04-19 12:00:00 +0200

    In 1869 the waffle iron was invented for people who had wrinkled waffles. ... Added line 2023 Id phJmmkWeQ5s= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2020,3 +2020,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 13:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV4][OUT][BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
