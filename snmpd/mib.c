ea73dfb38d5d8e1fbd0a9a3ef42f4454df96b429
Reyk Floeter <git@reykfloeter.com>
2012-04-20 12:00:00 +0200

    If I had a plantation in Georgia and a home in Hell, I'd sell the plantation and go home. -- Eugene P. Gallagher ... Added line 2026 Id 3WAtYuy7GsQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2023,3 +2023,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 14:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][IN][PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
