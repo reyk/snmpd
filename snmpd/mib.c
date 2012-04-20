1ae30d0dde3ac713d6e251d63a0689d740a6cf0f
Reyk Floeter <git@reykfloeter.com>
2012-04-20 12:00:00 +0200

    There is something fascinating about science. One gets such wholesale returns of conjecture out of such a trifling investment of fact. -- Mark Twain ... Added line 2027 Id vEx8iuj2KWI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2024,3 +2024,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 14:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][IN][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
