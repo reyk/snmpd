86d94d3c27e39283add408488ce3ba0805f90613
Reyk Floeter <git@reykfloeter.com>
2012-04-22 12:00:00 +0200

    Fortune's Real-Life Courtroom Quote #19: Q: Doctor, how many autopsies have you performed on dead people? A: All my autopsies have been performed on dead people. ... Added line 2034 Id wQRIwszy+bk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2031,3 +2031,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 16:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][IN][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
