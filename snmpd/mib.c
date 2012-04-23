2d014b86729866c319d440b86cb14bf920efd860
Reyk Floeter <git@reykfloeter.com>
2012-04-23 12:00:00 +0200

    Remember, drive defensively! And of course, the best defense is a good offense! ... Added line 2038 Id vxRpi17THkM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2035,3 +2035,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 17:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV6][IN][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
