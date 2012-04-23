a2ecda1e73cfeaecde6d547e7715f05c339c7301
Reyk Floeter <git@reykfloeter.com>
2012-04-23 12:00:00 +0200

    Accuracy, n.: The vice of being right. ... Added line 2039 Id x6q2JGczrlI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2036,3 +2036,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 17:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV6][IN][BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
