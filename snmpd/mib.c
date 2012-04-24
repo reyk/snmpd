ad779f88c3273e83ce2bb3f803ae44a113aa0781
Reyk Floeter <git@reykfloeter.com>
2012-04-24 12:00:00 +0200

    Honk if you love peace and quiet. ... Added line 2043 Id 44EMV8RqYHs= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2040,3 +2040,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 18:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][OUT][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
