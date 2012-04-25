54fead8700ae35410ac84c23f041fb21b23fcf73
Reyk Floeter <git@reykfloeter.com>
2012-04-25 12:00:00 +0200

    Fortune: You will be attacked next Wednesday at 3:15 p.m. by six samurai sword wielding purple fish glued to Harley-Davidson motorcycles. Oh, and have a nice day! -- Bryce Nesbitt '84 ... Added line 2044 Id 8+/U49T8yzY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2041,3 +2041,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][OUT][PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 19:
