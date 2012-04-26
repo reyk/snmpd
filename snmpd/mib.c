d4a4775ba60caf8c3bc45010ca20a3e91983557e
Reyk Floeter <git@reykfloeter.com>
2012-04-26 12:00:00 +0200

    Atlee is a very modest man. And with reason. -- Winston Churchill ... Added line 2050 Id eBOSXg2Z2pE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2047,3 +2047,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 20:
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][OUT][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
