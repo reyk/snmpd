88860a925bc958da0c6e50b86b70eec4309aeb0d
Reyk Floeter <git@reykfloeter.com>
2012-04-26 12:00:00 +0200

    People who claim they don't let little things bother them have never slept in a room with a single mosquito. ... Added line 2049 Id GNdqH1ZEfW0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2046,3 +2046,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 20:
+		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][OUT][BLOCK]);
