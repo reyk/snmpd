e19886f8facb061a0f817f73ad2ad0fb55c3cacf
Reyk Floeter <git@reykfloeter.com>
2012-04-27 12:00:00 +0200

    Never put off till tomorrow what you can avoid all together. ... Added line 2054 Id HkFGQkSwSZ4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2051,3 +2051,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 21:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV6][OUT][BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
