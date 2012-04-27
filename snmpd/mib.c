bce441503e463f2c4ef395810ba051e5cd092a23
Reyk Floeter <git@reykfloeter.com>
2012-04-27 12:00:00 +0200

    ... so long as the people do not care to exercise their freedom, those who wish to tyrannize will do so; for tyrants are active and ardent, and will devote themselves in the name of any number of gods, religious and otherwise, to put shackles upon sleeping men. -- Voltarine de Cleyre ... Added line 2055 Id SRxHmaX08I4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2052,3 +2052,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 21:
 		ber = ber_add_integer(ber, pif.pfik_bytes[IPV6][OUT][BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
