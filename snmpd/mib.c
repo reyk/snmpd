d166dcc3b39ff1a306c80a1db2907d798cda8b4c
Reyk Floeter <git@reykfloeter.com>
2012-04-27 12:00:00 +0200

    Sattinger's Law: It works better if you plug it in. ... Added line 2052 Id MB5iLOgk6oA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2049,3 +2049,4 @@ mib_pfiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, pif.pfik_packets[IPV6][OUT][BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 21:
