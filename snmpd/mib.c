1c1c08a357e83168d96cae4a9ee726afce215697
Reyk Floeter <git@reykfloeter.com>
2012-05-12 12:00:00 +0200

    Life would be much simpler and things would get done much faster if it weren't for other people -- Blore ... Added line 2112 Id l0HWVkeLxsA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2109,3 +2109,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, ts.pfrts_match);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 7:
