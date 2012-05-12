9aeb2498a7f8c3179707110099ababc79b20ddad
Reyk Floeter <git@reykfloeter.com>
2012-05-12 12:00:00 +0200

    Pure drivel tends to drive ordinary drivel off the TV screen. ... Added line 2114 Id 63v6LL5f9fI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2111,3 +2111,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 7:
 		ber = ber_add_integer(ber, ts.pfrts_nomatch);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
