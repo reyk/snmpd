1f90def5ecb6f70d6e9806cd133e1d3af84d79e7
Reyk Floeter <git@reykfloeter.com>
2012-05-14 12:00:00 +0200

    Speak softly and carry a +6 two-handed sword. ... Added line 2121 Id C5zVl3MdQXs= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2118,3 +2118,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 9:
+		ber = ber_add_integer(ber, ts.pfrts_bytes[IN][PFR_OP_PASS]);
