03c1dfdd433fd0b0dac6f8624a53a106c01754c5
Reyk Floeter <git@reykfloeter.com>
2012-05-14 12:00:00 +0200

    Why is it that we rejoice at a birth and grieve at a funeral? It is because we are not the person involved. -- Mark Twain ... Added line 2122 Id okXioyEb21w= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2119,3 +2119,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 9:
 		ber = ber_add_integer(ber, ts.pfrts_bytes[IN][PFR_OP_PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
