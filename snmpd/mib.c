056144b0fdd7947537a8452b3ca46e49a4607379
Reyk Floeter <git@reykfloeter.com>
2012-05-15 12:00:00 +0200

    I appreciate the fact that this draft was done in haste, but some of the sentences that you are sending out in the world to do your work for you are loitering in taverns or asleep beside the highway. -- Dr. Dwight Van de Vate, Professor of Philosophy, University of Tennessee at Knoxville ... Added line 2125 Id Z2jwjN/oykA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2122,3 +2122,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 10:
+		ber = ber_add_integer(ber, ts.pfrts_packets[IN][PFR_OP_BLOCK]);
