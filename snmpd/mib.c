b9dad6e213ed6936bb67c63e4ab49c449becbd6f
Reyk Floeter <git@reykfloeter.com>
2012-05-16 12:00:00 +0200

    Fine day to work off excess energy. Steal something heavy. ... Added line 2130 Id alDGlFtfILk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2127,3 +2127,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 11:
 		ber = ber_add_integer(ber, ts.pfrts_bytes[IN][PFR_OP_BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
