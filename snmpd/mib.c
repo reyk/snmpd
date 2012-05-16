4199cb4ec5694f86457c6998370eb95fefb98271
Reyk Floeter <git@reykfloeter.com>
2012-05-16 12:00:00 +0200

    As far as the laws of mathematics refer to reality, they are not certain, and as far as they are certain, they do not refer to reality. -- Albert Einstein ... Added line 2129 Id Dgwl3pQ1PoI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2126,3 +2126,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 11:
+		ber = ber_add_integer(ber, ts.pfrts_bytes[IN][PFR_OP_BLOCK]);
