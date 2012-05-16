71c948ceb70fb420ec1326077bdb4f16a56e9416
Reyk Floeter <git@reykfloeter.com>
2012-05-16 12:00:00 +0200

    People will do tomorrow what they did today because that is what they did yesterday. ... Added line 2128 Id vxyygdf6N+o= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2125,3 +2125,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, ts.pfrts_packets[IN][PFR_OP_BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 11:
