818da439a92a9d222f2b13b37a9fdef503271d67
Reyk Floeter <git@reykfloeter.com>
2012-05-17 12:00:00 +0200

    "There is nothing which cannot be answered by means of my doctrine," said a monk, coming into a teahouse where Nasrudin sat. "And yet just a short time ago, I was challenged by a scholar with an unanswerable question," said Nasrudin. "I could have answered it if I had been there." "Very well. He asked, `Why are you breaking into my house in the middle of the night?'" ... Added line 2134 Id V6bHhd2Lh+M= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2131,3 +2131,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 12:
 		ber = ber_add_integer(ber, ts.pfrts_packets[IN][PFR_OP_XPASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
