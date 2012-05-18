00dd041add9281467797e95cda0fc1a0d71ced33
Reyk Floeter <git@reykfloeter.com>
2012-05-18 12:00:00 +0200

    CChheecckk yyoouurr dduupplleexx sswwiittcchh.. ... Added line 2139 Id wOoP5EvvznU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2136,3 +2136,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 13:
 		ber = ber_add_integer(ber, ts.pfrts_bytes[IN][PFR_OP_XPASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
