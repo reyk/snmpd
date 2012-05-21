c77eecf06ce65ba31f701168ffa795b6522d338d
Reyk Floeter <git@reykfloeter.com>
2012-05-21 12:00:00 +0200

    Although golf was originally restricted to wealthy, overweight Protestants, today it's open to anybody who owns hideous clothing. -- Dave Barry ... Added line 2149 Id SqSc3Vuk0zU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2146,3 +2146,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 16:
+		ber = ber_add_integer(ber, ts.pfrts_packets[OUT][PFR_OP_BLOCK]);
