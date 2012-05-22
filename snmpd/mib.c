957f7bcd0fd3e276dd69fe1c6f40f10c29cae01e
Reyk Floeter <git@reykfloeter.com>
2012-05-22 12:00:00 +0200

    When the Universe was not so out of whack as it is today, and all the stars were lined up in their proper places, you could easily count them from left to right, or top to bottom, and the larger and bluer ones were set apart, and the smaller yellowing types pushed off to the corners as bodies of a lower grade ... -- Stanislaw Lem, "Cyberiad" ... Added line 2154 Id CZ+aTI4ovlA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2151,3 +2151,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 17:
 		ber = ber_add_integer(ber, ts.pfrts_bytes[OUT][PFR_OP_BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
