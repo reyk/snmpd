3dd575c7c1145c8862441cf275f73b6a402bfd5d
Reyk Floeter <git@reykfloeter.com>
2012-05-23 12:00:00 +0200

    Lackland's Laws: (1) Never be first. (2) Never be last. (3) Never volunteer for anything. ... Added line 2156 Id NqAABA2umKg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2153,3 +2153,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, ts.pfrts_bytes[OUT][PFR_OP_BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 18:
