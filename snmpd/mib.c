5c4239c35e0de71edaff189bd6ef1e73d428d573
Reyk Floeter <git@reykfloeter.com>
2012-05-23 12:00:00 +0200

    Mr. Cole's Axiom: The sum of the intelligence on the planet is a constant; the population is growing. ... Added line 2158 Id YRYe5n/RDmg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2155,3 +2155,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 18:
 		ber = ber_add_integer(ber, ts.pfrts_packets[OUT][PFR_OP_XPASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
