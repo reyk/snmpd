d88f9d831576d7bfdf9067ff7a7e768e31219bc9
Reyk Floeter <git@reykfloeter.com>
2012-05-23 12:00:00 +0200

    San Francisco isn't what it used to be, and it never was. -- Herb Caen ... Added line 2159 Id IO5IqhvoKX8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2156,3 +2156,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 18:
 		ber = ber_add_integer(ber, ts.pfrts_packets[OUT][PFR_OP_XPASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
