f873b9e56e9054203c13a6092c893555709bd1a4
Reyk Floeter <git@reykfloeter.com>
2012-05-23 12:00:00 +0200

    One day the King decided that he would force all his subjects to tell the truth. A gallows was erected in front of the city gates. A herald announced, "Whoever would enter the city must first answer the truth to a question which will be put to him." Nasrudin was first in line. The captain of the guard asked him, "Where are you going? Tell the truth -- the alternative is death by hanging." "I am going," said Nasrudin, "to be hanged on that gallows." "I don't believe you." "Very well, if I have told a lie, then hang me!" "But that would make it the truth!" "Exactly," said Nasrudin, "your truth." ... Added line 2157 Id fEy7w0uLCMI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2154,3 +2154,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 18:
+		ber = ber_add_integer(ber, ts.pfrts_packets[OUT][PFR_OP_XPASS]);
