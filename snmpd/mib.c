61be27e551d4cf87007595968e42bcd953c1a69b
Reyk Floeter <git@reykfloeter.com>
2012-05-24 12:00:00 +0200

    In those days he was wiser than he is now -- he used to frequently take my advice. -- Winston Churchill ... Added line 2161 Id +jHHkdq1T/Q= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2158,3 +2158,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 19:
+		ber = ber_add_integer(ber, ts.pfrts_bytes[OUT][PFR_OP_XPASS]);
