c9507a2010c8abf22080fd0239d621fa64a1f4df
Reyk Floeter <git@reykfloeter.com>
2012-05-24 12:00:00 +0200

    Yield to Temptation ... it may not pass your way again. -- Lazarus Long, "Time Enough for Love" ... Added line 2163 Id d/0n6A4dLS0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2160,3 +2160,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 19:
 		ber = ber_add_integer(ber, ts.pfrts_bytes[OUT][PFR_OP_XPASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
