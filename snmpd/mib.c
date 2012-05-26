307dac6b2821fe2602f71b0fb9014150ac7f8625
Reyk Floeter <git@reykfloeter.com>
2012-05-26 12:00:00 +0200

    If all the Chinese simultaneously jumped into the Pacific off a 10 foot platform erected 10 feet off their coast, it would cause a tidal wave that would destroy everything in this country west of Nebraska. ... Added line 2169 Id Ee6Unk99RVA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2166,3 +2166,4 @@ mib_pftables(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, tzero);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
 		break;
+	default:
