a9d7b81bc80d46067cec79669a827893f89b88fb
Reyk Floeter <git@reykfloeter.com>
2012-06-02 12:00:00 +0200

    HOW YOU CAN TELL THAT IT'S GOING TO BE A ROTTEN DAY: #15 Your pet rock snaps at you. ... Added line 2196 Id yksEydBfwoE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2193,3 +2193,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	switch (o->bo_id[OIDIDX_pfTblAddr]) {
 	case 1:
 		ber = ber_add_integer(ber, tblidx);
+		break;
