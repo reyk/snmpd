e8d8d9d80cf304a64ce9540feec95d25fb3b0c97
Reyk Floeter <git@reykfloeter.com>
2012-06-04 12:00:00 +0200

    After all, what is your hosts' purpose in having a party? Surely not for you to enjoy yourself; if that were their sole purpose, they'd have simply sent champagne and women over to your place by taxi. -- P. J. O'Rourke ... Added line 2207 Id VgqMZBJ4yDg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2204,3 +2204,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 4:
 		ber = ber_add_integer(ber, (time(NULL) - as.pfras_tzero) * 100);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
