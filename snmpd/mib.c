66f47023a425ff6038cd0ea041dafe0a3d37aa78
Reyk Floeter <git@reykfloeter.com>
2012-06-05 12:00:00 +0200

    Real software engineers don't like the idea of some inexplicable and greasy hardware several aisles away that may stop working at any moment. They have a great distrust of hardware people, and wish that systems could be virtual at *___all* levels. They would like personal computers (you know no one's going to trip over something and kill your DFA in mid-transit), except that they need 8 megabytes to run their Correctness Verification Aid packages. ... Added line 2208 Id 0QrVYuMWBmA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2205,3 +2205,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 4:
 		ber = ber_add_integer(ber, (time(NULL) - as.pfras_tzero) * 100);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
+		break;
