9b4f3450239ce600c0f3c43279a44a03be188e13
Reyk Floeter <git@reykfloeter.com>
2012-06-05 12:00:00 +0200

    Most people wouldn't know music if it came up and bit them on the ass. -- Frank Zappa ... Added line 2209 Id zw5yRgPe274= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2206,3 +2206,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, (time(NULL) - as.pfras_tzero) * 100);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_TIMETICKS);
 		break;
+	case 5:
