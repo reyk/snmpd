d7ab34c3f8565c83ca4735d8681fbbd4c972f795
Reyk Floeter <git@reykfloeter.com>
2012-06-06 12:00:00 +0200

    Steele's Plagiarism of Somebody's Philosophy: Everybody should believe in something -- I believe I'll have another drink. ... Added line 2213 Id IEo0UC+6aEg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2210,3 +2210,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, as.pfras_packets[IN][PFR_OP_BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 6:
