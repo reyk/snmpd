df1af0dec11ea25497718433874b7ccf95937088
Reyk Floeter <git@reykfloeter.com>
2012-06-07 12:00:00 +0200

    "At a recent meeting in Snowmass, Colorado, a participant from Los Angeles fainted from hyperoxygenation, and we had to hold his head under the exhaust of a bus until he revived." ... Added line 2219 Id LkZqrghcV5M= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2216,3 +2216,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 7:
 		ber = ber_add_integer(ber, as.pfras_packets[IN][PFR_OP_PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
