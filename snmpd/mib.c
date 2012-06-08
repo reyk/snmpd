241be4a9f7e5d6cdbd85acbeed284efe1afa8f1e
Reyk Floeter <git@reykfloeter.com>
2012-06-08 12:00:00 +0200

    It would be nice if the Food and Drug Administration stopped issuing warnings about toxic substances and just gave me the names of one or two things still safe to eat. -- Robert Fuoss ... Added line 2220 Id kUoA1ANeN6E= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2217,3 +2217,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 7:
 		ber = ber_add_integer(ber, as.pfras_packets[IN][PFR_OP_PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
