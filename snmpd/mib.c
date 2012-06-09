fa0f12b3ef0db102c8889ce1c5417206fa181ef6
Reyk Floeter <git@reykfloeter.com>
2012-06-09 12:00:00 +0200

    Support your local police force -- steal!! ... Added line 2227 Id AzPsqooMUCo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2224,3 +2224,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 9:
 		ber = ber_add_integer(ber, as.pfras_packets[OUT][PFR_OP_BLOCK]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
