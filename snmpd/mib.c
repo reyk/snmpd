dcfef68cbc5cb02d6b6448fd72dfcdbf7e807bd4
Reyk Floeter <git@reykfloeter.com>
2012-06-11 12:00:00 +0200

    Conscience is what hurts when everything else feels so good. ... Added line 2233 Id MCg3ga7RI6c= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2230,3 +2230,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, as.pfras_bytes[OUT][PFR_OP_BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 11:
