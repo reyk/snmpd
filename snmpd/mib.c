7d83e099c6df1fcda6ad32ef0d0338989e900ce9
Reyk Floeter <git@reykfloeter.com>
2012-06-11 12:00:00 +0200

    Faith, n: That quality which enables us to believe what we know to be untrue. ... Added line 2232 Id ajiKxgAzNi0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2229,3 +2229,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 10:
 		ber = ber_add_integer(ber, as.pfras_bytes[OUT][PFR_OP_BLOCK]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
