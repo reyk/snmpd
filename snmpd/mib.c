81a51b86b5c799387672c842540de4fff2fd79d4
Reyk Floeter <git@reykfloeter.com>
2012-06-12 12:00:00 +0200

    It is against the grain of modern education to teach children to program. What fun is there in making plans, acquiring discipline in organizing thoughts, devoting attention to detail, and learning to be self-critical? -- Alan Perlis ... Added line 2239 Id nopohcUVg2Y= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2236,3 +2236,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 12:
 		ber = ber_add_integer(ber, as.pfras_bytes[OUT][PFR_OP_PASS]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
