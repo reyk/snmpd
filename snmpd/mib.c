b051eeb7d5ff1e2827b60c82ec3b84c630976287
Reyk Floeter <git@reykfloeter.com>
2012-06-12 12:00:00 +0200

    Nature abhors a hero. For one thing, he violates the law of conservation of energy. For another, how can it be the survival of the fittest when the fittest keeps putting himself in situations where he is most likely to be creamed? -- Solomon Short ... Added line 2237 Id 3Y2mm330Ils= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2234,3 +2234,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, as.pfras_packets[OUT][PFR_OP_PASS]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 12:
