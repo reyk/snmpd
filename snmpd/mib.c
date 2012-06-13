2d93c317cf59de731f5ff67aad2ed35987a269b4
Reyk Floeter <git@reykfloeter.com>
2012-06-13 12:00:00 +0200

    May the Fleas of a Thousand Camels infest one of your Erogenous Zones. ... Added line 2242 Id 6pfAw2KOwVo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2239,3 +2239,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	default:
+		return (-1);
