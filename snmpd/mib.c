a39d100f459b8736cfdb190ef9a6d8244445eba9
Reyk Floeter <git@reykfloeter.com>
2012-06-15 12:00:00 +0200

    Kirkland, Illinois, law forbids bees to fly over the village or through any of its streets. ... Added line 2251 Id XKJXQ4Hr9ho= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2248,3 +2248,4 @@ mib_pftableaddrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 struct ber_oid *
 mib_pftableaddrstable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 {
+	struct pfr_astats	 as;
