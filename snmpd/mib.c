855b0ffa3d00e1fa4717bc4926912688ee1e056c
Reyk Floeter <git@reykfloeter.com>
2012-06-20 12:00:00 +0200

    I couldn't remember when I had been so disappointed. Except perhaps the time I found out that M&Ms really *do* melt in your hand ... -- Peter Oakley ... Added line 2270 Id qxn1w/Audpo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2267,3 +2267,4 @@ mib_pftableaddrstable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 		b.o_oidlen--;
 		b.o_flags |= OID_TABLE;
 		if (smi_oid_cmp(&a, &b) == 0) {
+			o->bo_id[id] = oid->o_oid[id];
