848268567f1a2a8fab90558bbbd787d02b60d3d7
Reyk Floeter <git@reykfloeter.com>
2012-06-24 12:00:00 +0200

    On two occasions I have been asked [by members of Parliament!], `Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?' I am not able rightly to apprehend the kind of confusion of ideas that could provoke such a question. -- Charles Babbage ... Added line 2287 Id uuS2dOCNwls= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2284,3 +2284,4 @@ mib_pftableaddrstable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 		if (pfta_get_nextaddr(&as, &tblidx)) {
 			/* We reached the last addr in the last table.
 			 * When the next OIDIDX_pfTblAddr'th OID is requested,
+			 * get the first table address again.
