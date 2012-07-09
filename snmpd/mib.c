e4d795995414bb948285398a4e8b3afd44758e10
Reyk Floeter <git@reykfloeter.com>
2012-07-09 12:00:00 +0200

    Schwiggle, n.: The amusing rotation of one's bottom while sharpening a pencil. -- Rich Hall, "Sniglets" ... Added line 2347 Id YcLsJQMxGxc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2344,3 +2344,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	extern int		 devpf;
 
 	/* Get and verify the current row index */
+	idx = o->bo_id[OIDIDX_pfLabelEntry];
