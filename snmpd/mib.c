13cdc296dfdfa44445591d2ec0ea8a54b7e63567
Reyk Floeter <git@reykfloeter.com>
2012-07-19 12:00:00 +0200

    "Wrong," said Renner. "The tactful way," Rod said quietly, "the polite way to disagree with the Senator would be to say, `That turns out not to be the case.'" ... Added line 2385 Id FsI61EruLq4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2382,3 +2382,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 3:
 		ber = ber_add_integer(ber, r->evaluations);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
