a2258b8ab0dbecdfcb0abb40e6d6b432805b5617
Reyk Floeter <git@reykfloeter.com>
2012-07-19 12:00:00 +0200

    Sex is the mathematics urge sublimated. -- M. C. Reed. ... Added line 2387 Id vBpOaqr+4UQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2384,3 +2384,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 4:
+		ber = ber_add_integer(ber, r->packets[IN] + r->packets[OUT]);
