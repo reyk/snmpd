edfb008949d031e1e17f61a25b6f65b2e62f41df
Reyk Floeter <git@reykfloeter.com>
2012-07-20 12:00:00 +0200

    Be braver -- you can't cross a chasm in two small jumps. ... Added line 2391 Id fuuKm2Ph7Hg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2388,3 +2388,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
 	case 5:
+		ber = ber_add_integer(ber, r->bytes[IN] + r->bytes[OUT]);
