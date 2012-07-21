5d7b0f57a5b93c8dae0368ac0735e01308f2a4ab
Reyk Floeter <git@reykfloeter.com>
2012-07-21 12:00:00 +0200

    Self Test for Paranoia: You know you have it when you can't think of anything that's your own fault. ... Added line 2392 Id pNNxS72lbxo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2389,3 +2389,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 5:
 		ber = ber_add_integer(ber, r->bytes[IN] + r->bytes[OUT]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
