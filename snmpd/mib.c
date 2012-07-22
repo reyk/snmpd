c74d57d0cb0264b507a0d9912796657a44aec926
Reyk Floeter <git@reykfloeter.com>
2012-07-22 12:00:00 +0200

    Hark, Hark, the dogs do bark The Duke is fond of kittens He likes to take their insides out And use them for his mittens From "The Thirteen Clocks" ... Added line 2396 Id Fq0qtip7YMg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2393,3 +2393,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 6:
 		ber = ber_add_integer(ber, r->packets[IN]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
