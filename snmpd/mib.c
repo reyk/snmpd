50cc1aa7602fb23e5b446f49ad210fbc0dd23465
Reyk Floeter <git@reykfloeter.com>
2012-07-23 12:00:00 +0200

    Goto, n.: A programming tool that exists to allow structured programmers to complain about unstructured programmers. -- Ray Simard ... Added line 2400 Id FjX7UwF7yLo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2397,3 +2397,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 7:
 		ber = ber_add_integer(ber, r->bytes[IN]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
