52e91deca50f4f4f04983e13664fef3ba6afe5b7
Reyk Floeter <git@reykfloeter.com>
2012-07-23 12:00:00 +0200

    Hatred, n.: A sentiment appropriate to the occasion of another's superiority. -- Ambrose Bierce, "The Devil's Dictionary" ... Added line 2401 Id I2y1NmLUeag= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2398,3 +2398,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 7:
 		ber = ber_add_integer(ber, r->bytes[IN]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
