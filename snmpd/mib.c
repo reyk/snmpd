e462260f24a92c8b0e293f226c7344e3b25ef2bc
Reyk Floeter <git@reykfloeter.com>
2012-07-24 12:00:00 +0200

    They spell it "da Vinci" and pronounce it "da Vinchy". Foreigners always spell better than they pronounce. -- Mark Twain ... Added line 2406 Id OEaCw91eXJ8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2403,3 +2403,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, r->packets[OUT]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 9:
