329ab612d8acb2244101135de9efd435e10e2e50
Reyk Floeter <git@reykfloeter.com>
2012-07-25 12:00:00 +0200

    Baker's First Law of Federal Geometry: A block grant is a solid mass of money surrounded on all sides by governors. ... Added line 2408 Id WG59LseoFKQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2405,3 +2405,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 9:
 		ber = ber_add_integer(ber, r->bytes[OUT]);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
