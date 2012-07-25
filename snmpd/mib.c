859ab4580e1264d3dd6474f09f73267cf2a71fc1
Reyk Floeter <git@reykfloeter.com>
2012-07-25 12:00:00 +0200

    Carelessly planned projects take three times longer to complete than expected. Carefully planned projects take four times longer to complete than expected, mostly because the planners expect their planning to reduce the time it takes. ... Added line 2410 Id tn3SrH7CkBU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2407,3 +2407,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, r->bytes[OUT]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
 		break;
+	case 10:
