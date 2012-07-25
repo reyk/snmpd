53ac1d2e8c6f24836c18c8cc55fe9520afdc5596
Reyk Floeter <git@reykfloeter.com>
2012-07-25 12:00:00 +0200

    When the weight of the paperwork equals the weight of the plane, the plane will fly. -- Donald Douglas ... Added line 2409 Id BSAEAUq0wU8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2406,3 +2406,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 9:
 		ber = ber_add_integer(ber, r->bytes[OUT]);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER64);
+		break;
