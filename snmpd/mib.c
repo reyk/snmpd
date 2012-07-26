2089b8ad83d6147900d3cff08be333975454d46b
Reyk Floeter <git@reykfloeter.com>
2012-07-26 12:00:00 +0200

    We are confronted with insurmountable opportunities. -- Walt Kelly, "Pogo" ... Added line 2412 Id +BvoHNGGFgg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2409,3 +2409,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 10:
 		ber = ber_add_integer(ber, r->states_tot);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
