5bcd827a2d5ff2805bd73c329ec5de99c400e391
Reyk Floeter <git@reykfloeter.com>
2012-07-26 12:00:00 +0200

    CChheecckk yyoouurr dduupplleexx sswwiittcchh.. ... Added line 2414 Id 9VyXKUSqCKo= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2411,3 +2411,4 @@ mib_pflabels(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, r->states_tot);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	default:
