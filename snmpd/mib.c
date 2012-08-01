9867c45cc75f7ec46e3e8d430713afed8de131c2
Reyk Floeter <git@reykfloeter.com>
2012-08-01 12:00:00 +0200

    Every program has two purposes -- one for which it was written and another for which it wasn't. ... Added line 2437 Id DHWd+M8F4jk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2434,3 +2434,4 @@ mib_pfsyncstats(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		{ 2, &s.pfsyncs_ipackets6 },
 		{ 3, &s.pfsyncs_badif },
 		{ 4, &s.pfsyncs_badttl },
+		{ 5, &s.pfsyncs_hdrops },
