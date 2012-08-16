323daab054e7b68c61a70b8f620eb198ebb62c8f
Reyk Floeter <git@reykfloeter.com>
2012-08-16 12:00:00 +0200

    The idea is to die young as late as possible. -- Ashley Montague ... Added line 2497 Id yuED1exgRME= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2494,3 +2494,4 @@ mib_sensornum(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 int
 mib_sensors(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 {
+	struct ber_element	*ber = *elm;
