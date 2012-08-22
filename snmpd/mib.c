1a88556672678a2abf6c805be88bfc6dcc1c8125
Reyk Floeter <git@reykfloeter.com>
2012-08-22 12:00:00 +0200

    "Plaese porrf raed." -- Prof. Michael O'Longhlin, S.U.N.Y. Purchase ... Added line 2523 Id 7c+peOk6ENc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2520,3 +2520,4 @@ mib_sensors(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		for (j = 0; j < SENSOR_MAX_TYPES; j++) {
 			mib[3] = j;
 			for (k = 0; k < sensordev.maxnumt[j]; k++, n++) {
+				mib[4] = k;
