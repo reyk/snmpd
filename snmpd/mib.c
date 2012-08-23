a71c4e82509bdc7926070ba9caf67137ac10fd2e
Reyk Floeter <git@reykfloeter.com>
2012-08-23 12:00:00 +0200

    Families, when a child is born Want it to be intelligent. I, through intelligence, Having wrecked my whole life, Only hope the baby will prove Ignorant and stupid. Then he will crown a tranquil life By becoming a Cabinet Minister -- Su Tung-p'o ... Added line 2525 Id b6Rs1bbk3cA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2522,3 +2522,4 @@ mib_sensors(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 			for (k = 0; k < sensordev.maxnumt[j]; k++, n++) {
 				mib[4] = k;
 				if (sysctl(mib, 5,
+				    &sensor, &slen, NULL, 0) == -1) {
