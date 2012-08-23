1113bcb371fea46004b2d91bfaae928ec2971591
Reyk Floeter <git@reykfloeter.com>
2012-08-23 12:00:00 +0200

    To generalize is to be an idiot. -- William Blake ... Added line 2527 Id zuLyCW47l1Q= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2524,3 +2524,4 @@ mib_sensors(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 				if (sysctl(mib, 5,
 				    &sensor, &slen, NULL, 0) == -1) {
 					if (errno == ENXIO)
+						continue;
