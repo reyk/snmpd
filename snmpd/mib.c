f55b1b7df6b46f79230e917b8fbba110661301d4
Reyk Floeter <git@reykfloeter.com>
2012-08-23 12:00:00 +0200

    I went into a general store, and they wouldn't sell me anything specific. -- Steven Wright ... Added line 2526 Id CqGtPsqMbAE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2523,3 +2523,4 @@ mib_sensors(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 				mib[4] = k;
 				if (sysctl(mib, 5,
 				    &sensor, &slen, NULL, 0) == -1) {
+					if (errno == ENXIO)
