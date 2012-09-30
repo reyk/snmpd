16e8b2cac1c2b7698eb212a4871f9c8c4a4eed05
Reyk Floeter <git@reykfloeter.com>
2012-09-30 12:00:00 +0200

    Gray's Law of Programming: `_n+1' trivial tasks are expected to be accomplished in the same time as `_n' tasks. Logg's Rebuttal to Gray's Law: `_n+1' trivial tasks take twice as long as `_n' trivial tasks. ... Added line 2677 Id E7vI/2TAxLM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2674,3 +2674,4 @@ mib_carpstats(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	int			 mib[] = { CTL_NET, PF_INET, IPPROTO_CARP,
 				    CARPCTL_STATS };
 	size_t			 len;
+	struct			 carpstats stats;
