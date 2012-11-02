b532f4523d95dfdd701fd6392cf16deaad5f2bef
Reyk Floeter <git@reykfloeter.com>
2012-11-02 12:00:00 +0100

    Help fight continental drift. ... Added line 2809 Id b9klEw/0w1c= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2806,3 +2806,4 @@ mib_carpiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	/* Tables need to prepend the OID on their own */
 	o->bo_id[OIDIDX_carpIfEntry] = cif->kif.if_index;
 	*elm = ber_add_oid(*elm, o);
+
