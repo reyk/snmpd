d71a68d032b690f96fd21f7918392b54a17fd72d
Reyk Floeter <git@reykfloeter.com>
2012-11-11 12:00:00 +0100

    A "No" uttered from deepest conviction is better and greater than a "Yes" merely uttered to please, or what is worse, to avoid trouble. -- Mahatma Ghandi ... Added line 2844 Id uo0u/9d4TgU= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2841,3 +2841,4 @@ mib_carpiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 int
 mib_memiftable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 {
+	struct ber_element	*ber = *elm;
