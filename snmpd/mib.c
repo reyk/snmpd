fcd7b864ef90280b8e43d9bc111a535ae5484fa2
Reyk Floeter <git@reykfloeter.com>
2012-11-21 12:00:00 +0100

    FORTUNE'S PARTY TIPS #14 Tired of finding that other people are helping themselves to your good liquor at BYOB parties? Take along a candle, which you insert and light after you've opened the bottle. No one ever expects anything drinkable to be in a bottle which has a candle stuck in its neck. ... Added line 2886 Id aLp+QCC1dSE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2883,3 +2883,4 @@ int mib_ipreasmfails(struct oid *, struct ber_oid *, struct ber_element **);
 int mib_ipfragfails(struct oid *, struct ber_oid *, struct ber_element **);
 int mib_iproutingdiscards(struct oid *, struct ber_oid *,
     struct ber_element **);
+int mib_ipaddr(struct oid *, struct ber_oid *, struct ber_element **);
