bd2cc3ac8134f26c0e748edc5944289bfee28077
Reyk Floeter <git@reykfloeter.com>
2012-11-30 12:00:00 +0100

    Philadelphia is not dull -- it just seems so because it is next to exciting Camden, New Jersey. ... Added line 2921 Id /Xi9CzXOMrY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2918,3 +2918,4 @@ static struct oid ip_mib[] = {
 	{ MIB(ipAdEntNetMask),		OID_TRD, mib_ipaddr, NULL,
 	    mib_ipaddrtable },
 	{ MIB(ipAdEntBcastAddr),	OID_TRD, mib_ipaddr, NULL,
+	    mib_ipaddrtable },
