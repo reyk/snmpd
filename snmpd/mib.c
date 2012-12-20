e9446ae366fafe36fda90a76b4f982fb1447281d
Reyk Floeter <git@reykfloeter.com>
2012-12-20 12:00:00 +0100

    It's not so hard to lift yourself by your bootstraps once you're off the ground. -- Daniel B. Luten ... Added line 3002 Id iFr2+cMGhOA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -2999,3 +2999,4 @@ mib_ipstat(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		if (oid->o_oid[OIDIDX_ip] == mapping[i].m_id) {
 			*elm = ber_add_integer(*elm, *mapping[i].m_ptr);
 			ber_set_header(*elm,
+			    BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
