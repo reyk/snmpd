9ef16b397b3973b7ac2f8cacd15ece1ce7a48da4
Reyk Floeter <git@reykfloeter.com>
2012-12-30 12:00:00 +0100

    Two percent of zero is almost nothing. ... Added line 3042 Id OFrDaMS2hUY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3039,3 +3039,4 @@ mib_ipinaddrerrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 
 	errors = ipstat.ips_cantforward + ipstat.ips_badaddr;
 
+	*elm = ber_add_integer(*elm, errors);
