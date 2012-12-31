92ed8fda01abc295d7076b87a97d9ea8b5fcc90e
Reyk Floeter <git@reykfloeter.com>
2012-12-31 12:00:00 +0100

    I haven't lost my mind; I know exactly where I left it. ... Added line 3045 Id NpIv4hV4KHk= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3042,3 +3042,4 @@ mib_ipinaddrerrs(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	*elm = ber_add_integer(*elm, errors);
 	ber_set_header(*elm, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 
+	return (0);
