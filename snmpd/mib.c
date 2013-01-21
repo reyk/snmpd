b2875592fdf9924a1ed3ad09323bd34472d1abb9
Reyk Floeter <git@reykfloeter.com>
2013-01-21 12:00:00 +0100

    Fine day to throw a party. Throw him as far as you can. ... Added line 3129 Id 9hD17xudlpA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3126,3 +3126,4 @@ mib_ipaddrtable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 	if (o->bo_n >= oid->o_oidlen) {
 		/*
 		 * Compare the requested and the matched OID to see
+		 * if we have to iterate to the next element.
