70a2e8adccbcdd94008b531f324f4b09e512028e
Reyk Floeter <git@reykfloeter.com>
2013-01-29 12:00:00 +0100

    "Deep" is a word like "theory" or "semantic" -- it implies all sorts of marvelous things. It's one thing to be able to say "I've got a theory", quite another to say "I've got a semantic theory", but, ah, those who can claim "I've got a deep semantic theory", they are truly blessed. -- Randy Davis ... Added line 3160 Id Z25njj9FJIc= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3157,3 +3157,4 @@ mib_ipaddrtable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 		/* Encode real IPv4 address */
 		addr.sin_addr.s_addr = ka->addr.sin.sin_addr.s_addr;
 		mps_encodeinaddr(no, &addr.sin_addr, OIDIDX_ipAddr + 1);
+	}
