b5c4fb5935ed5051cb54cd00fae31c27179c4189
Reyk Floeter <git@reykfloeter.com>
2013-02-14 12:00:00 +0100

    But scientists, who ought to know Assure us that it must be so. Oh, let us never, never doubt What nobody is sure about. -- Hilaire Belloc ... Added line 3224 Id uqdo8ezRFKY= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3221,3 +3221,4 @@ mib_ipaddr(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 int mib_ipfnroutes(struct oid *, struct ber_oid *, struct ber_element **);
 struct ber_oid *
 mib_ipfroutetable(struct oid *oid, struct ber_oid *o, struct ber_oid *no);
+int mib_ipfroute(struct oid *, struct ber_oid *, struct ber_element **);
