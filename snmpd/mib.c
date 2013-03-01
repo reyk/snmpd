a567ccdf2b50f64b1ddc7014eaf0764c3032fd9e
Reyk Floeter <git@reykfloeter.com>
2013-03-01 12:00:00 +0100

    Bathquake, n.: The violent quake that rattles the entire house when the water faucet is turned on to a certain point. -- Rich Hall, "Sniglets" ... Added line 3287 Id k+rLGOOy0GM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3284,3 +3284,4 @@ mib_ipfroutetable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 		 * Compare the requested and the matched OID to see
 		 * if we have to iterate to the next element.
 		 */
+		bzero(&a, sizeof(a));
