4af0b68e918cfe8334b005f464c6251e797e998f
Reyk Floeter <git@reykfloeter.com>
2013-03-02 12:00:00 +0100

    Fortune's Fictitious Country Song Title of the Week: "How Can I Miss You if You Won't Go Away?" ... Added line 3291 Id 7pucifdOXfE= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3288,3 +3288,4 @@ mib_ipfroutetable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 		bcopy(o, &a.o_id, sizeof(struct ber_oid));
 		bzero(&b, sizeof(b));
 		bcopy(&oid->o_id, &b.o_id, sizeof(struct ber_oid));
+		b.o_oidlen--;
