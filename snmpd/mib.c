a79fad08b061d659994fd66b1a580acac3f131d7
Reyk Floeter <git@reykfloeter.com>
2013-03-16 12:00:00 +0100

    I appreciate the fact that this draft was done in haste, but some of the sentences that you are sending out in the world to do your work for you are loitering in taverns or asleep beside the highway. -- Dr. Dwight Van de Vate, Professor of Philosophy, University of Tennessee at Knoxville ... Added line 3346 Id AVWqg7xnu70= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3343,3 +3343,4 @@ mib_ipfroutetable(struct oid *oid, struct ber_oid *o, struct ber_oid *no)
 	if (kr != NULL) {
 		no->bo_id[no->bo_n++] = atype;
 		no->bo_id[no->bo_n++] = 0x04;
+		mps_encodeinaddr(no, &kr->nexthop, no->bo_n);
