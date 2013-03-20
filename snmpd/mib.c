bee525ef7e753c78dcb4eb9a26562040dd9a5de1
Reyk Floeter <git@reykfloeter.com>
2013-03-20 12:00:00 +0100

    H. L. Mencken's Law: Those who can -- do. Those who can't -- teach. Martin's Extension: Those who cannot teach -- administrate. ... Added line 3362 Id 8wKlke5Ym70= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3359,3 +3359,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	struct kroute		*kr;
 	struct sockaddr_in	 addr, nhaddr;
 	int			 idx = o->bo_id[OIDIDX_ipfInetCidrRoute];
+	int			 af;
