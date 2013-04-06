deb05232f9cc98b0c327778d02f02c953f9343f8
Reyk Floeter <git@reykfloeter.com>
2013-04-06 12:00:00 +0200

    What's the use of a good quotation if you can't change it? -- Dr. Who ... Added line 3431 Id CMf+7JbCFaM= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3428,3 +3428,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, 0);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
 		break;
+	case 11: /* NextHopAS */
