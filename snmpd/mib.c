ae43b473fe91bbfaa62cb87ec86f69082cec7f68
Reyk Floeter <git@reykfloeter.com>
2013-04-06 12:00:00 +0200

    You couldn't even prove the White House staff sane beyond a reasonable doubt. -- Ed Meese, on the Hinckley verdict ... Added line 3429 Id vgBD6gVP+s8= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3426,3 +3426,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 10: /* Age */
 		ber = ber_add_integer(ber, 0);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
