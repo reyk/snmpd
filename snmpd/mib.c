9602e2f868534ea5fa5aa2aac7ba2cede424ac43
Reyk Floeter <git@reykfloeter.com>
2013-04-07 12:00:00 +0200

    Take heart amid the deepening gloom that your dog is finally getting enough cheese. -- National Lampoon, "Deteriorata" ... Added line 3434 Id UoZd5sB5/CA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3431,3 +3431,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 11: /* NextHopAS */
 		ber = ber_add_integer(ber, 0);	/* unknown */
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_GAUGE32);
+		break;
