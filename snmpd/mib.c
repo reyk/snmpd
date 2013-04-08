5b66b8905eb3b23b5d76b0e11beb097d07faa6c8
Reyk Floeter <git@reykfloeter.com>
2013-04-08 12:00:00 +0200

    Electrical Engineers do it with less resistance. ... Added line 3438 Id JAtTMCEUAHA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3435,3 +3435,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 12: /* Metric1 */
 		ber = ber_add_integer(ber, -1);	/* XXX */
 		break;
+	case 13: /* Metric2 */
