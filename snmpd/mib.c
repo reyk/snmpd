9ed60fd9c52352573363be398369e60dc6e2d161
Reyk Floeter <git@reykfloeter.com>
2013-04-11 12:00:00 +0200

    Abandon the search for Truth; settle for a good fantasy. ... Added line 3451 Id XQdSMRW0TH4= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3448,3 +3448,4 @@ mib_ipfroute(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, -1);	/* XXX */
 		break;
 	case 17: /* Status */
+		ber = ber_add_integer(ber, 1);	/* XXX */
