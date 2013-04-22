5774a7b69a7637f817c00859cbe9ba7330143e87
Reyk Floeter <git@reykfloeter.com>
2013-04-22 12:00:00 +0200

    Food for thought is no substitute for the real thing. -- Walt Kelly, "Putluck Pogo" ... Added line 3492 Id wQpx/WtVSXg= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3489,3 +3489,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	len = sizeof(diskcount);
 	mib[1] = HW_DISKCOUNT;
 	if (sysctl(mib, sizeofa(mib), &diskcount, &len, NULL, 0) == -1)
+		return (-1);
