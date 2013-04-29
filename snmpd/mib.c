aeadc60f0481b0d518d3f9983dab321f9f296c90
Reyk Floeter <git@reykfloeter.com>
2013-04-29 12:00:00 +0200

    Mitchell's Law of Committees: Any simple problem can be made insoluble if enough meetings are held to discuss it. ... Added line 3523 Id 87Ul61fdlhI= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3520,3 +3520,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		break;
 	case 3: /* diskIONRead */
 		ber = ber_add_integer(ber, (u_int32_t)stats[idx - 1].ds_rbytes);
+		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
