88f02ae044d4b08f06d031894660251dc979c116
Reyk Floeter <git@reykfloeter.com>
2013-04-30 12:00:00 +0200

    In Blythe, California, a city ordinance declares that a person must own at least two cows before he can wear cowboy boots in public. ... Added line 3526 Id /Rco6l44B18= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3523,3 +3523,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 4: /* diskIONWritten */
+		ber = ber_add_integer(ber, (u_int32_t)stats[idx - 1].ds_wbytes);
