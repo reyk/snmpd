b29053c7241e605cd3730319682a8c35dd1cfa86
Reyk Floeter <git@reykfloeter.com>
2013-04-30 12:00:00 +0200

    101 USES FOR A DEAD MICROPROCESSOR (1) Scarecrow for centipedes (2) Dead cat brush (3) Hair barrettes (4) Cleats (5) Self-piercing earrings (6) Fungus trellis (7) False eyelashes (8) Prosthetic dog claws . . . (99) Window garden harrow (pulled behind Tonka tractors) (100) Killer velcro (101) Currency ... Added line 3524 Id D5WCCFmOQMA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3521,3 +3521,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	case 3: /* diskIONRead */
 		ber = ber_add_integer(ber, (u_int32_t)stats[idx - 1].ds_rbytes);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
+		break;
