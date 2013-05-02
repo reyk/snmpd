edf92730b2491f36f3ba40fc0bf46d5398cc974c
Reyk Floeter <git@reykfloeter.com>
2013-05-02 12:00:00 +0200

    I cannot conceive that anybody will require multiplications at the rate of 40,000 or even 4,000 per hour ... -- F. H. Wales (1936) ... Added line 3534 Id 2wjvngMWN0M= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3531,3 +3531,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
 	case 6: /* diskIOWrites */
+		ber = ber_add_integer(ber, (u_int32_t)stats[idx - 1].ds_wxfer);
