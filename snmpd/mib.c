9cb5a90cf75804b4eaa578fb7237af3a0c4e58a8
Reyk Floeter <git@reykfloeter.com>
2013-05-02 12:00:00 +0200

    There can be no twisted thought without a twisted molecule. -- R. W. Gerard ... Added line 3533 Id gSIXaVx60PA= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3530,3 +3530,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, (u_int32_t)stats[idx - 1].ds_rxfer);
 		ber_set_header(ber, BER_CLASS_APPLICATION, SNMP_T_COUNTER32);
 		break;
+	case 6: /* diskIOWrites */
