a134d2aa81df2a3068a640b6af134f29939dba42
Reyk Floeter <git@reykfloeter.com>
2013-05-08 12:00:00 +0200

    This novel is not to be tossed lightly aside, but to be hurled with great force. -- Dorothy Parker ... Added line 3559 Id JUhG4MOJ+Co= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3556,3 +3556,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
  *
  * This MIB is required by some NMS to accept the device because
  * the RFC says that mostly any network device has to provide this MIB... :(
+ */
