bc834c40911ab7eab62054528bfb3dac7156f0bc
Reyk Floeter <git@reykfloeter.com>
2013-05-09 12:00:00 +0200

    When you try to make an impression, the chances are that is the impression you will make. ... Added line 3563 Id 9bKzpAQPn2g= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3560,3 +3560,4 @@ mib_diskio(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 
 int	 mib_dot1dtable(struct oid *, struct ber_oid *, struct ber_element **);
 
+static struct oid bridge_mib[] = {
