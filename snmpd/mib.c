a38cef20cfb73dbf31010db12f471127e9085898
Reyk Floeter <git@reykfloeter.com>
2013-05-17 12:00:00 +0200

    Cecil, you're my final hope Of finding out the true Straight Dope For I have been reading of Schrodinger's cat But none of my cats are at all like that. This unusual animal (so it is said) Is simultaneously alive and dead! What I don't understand is just why he Can't be one or the other, unquestionably. My future now hangs in between eigenstates. In one I'm enlightened, in the other I ain't. If *you* understand, Cecil, then show me the way And rescue my psyche from quantum decay. But if this queer thing has perplexed even you, Then I will *___and* I won't see you in Schrodinger's zoo. -- Randy F., Chicago, "The Straight Dope, a compendium of human knowledge" by Cecil Adams ... Added line 3592 Id w2zsb1ljHC0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3589,3 +3589,4 @@ mib_dot1dtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	/* Tables need to prepend the OID on their own */
 	o->bo_id[OIDIDX_dot1dEntry] = kif->if_index;
 	ber = ber_add_oid(ber, o);
+
