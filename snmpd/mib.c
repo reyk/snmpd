ef0a987a3bc17ac5c2c7db4a11fc32c73c2f6da0
Reyk Floeter <git@reykfloeter.com>
2013-05-18 12:00:00 +0200

    Reisner's Rule of Conceptual Inertia: If you think big enough, you'll never have to do it. ... Added line 3599 Id C2y2WCeoYf0= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3596,3 +3596,4 @@ mib_dot1dtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 		ber = ber_add_integer(ber, kif->if_index);
 		break;
 	case 3:
+		ber = ber_add_oid(ber, &zerodotzero);
