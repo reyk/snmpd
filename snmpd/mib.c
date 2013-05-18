b9cef185db70098f5423de297ef679f7d7f15014
Reyk Floeter <git@reykfloeter.com>
2013-05-18 12:00:00 +0200

    Maryel brought her bat into Exit once and started whacking people on the dance floor. Now everyone's doing it. It's called grand slam dancing. -- Ransford, Chicago Reader 10/7/83 ... Added line 3596 Id 5WPaHAtMQgQ= to mib.c.

--- snmpd/mib.c
+++ snmpd/mib.c
@@ -3593,3 +3593,4 @@ mib_dot1dtable(struct oid *oid, struct ber_oid *o, struct ber_element **elm)
 	switch (o->bo_id[OIDIDX_dot1d]) {
 	case 1:
 	case 2:
+		ber = ber_add_integer(ber, kif->if_index);
